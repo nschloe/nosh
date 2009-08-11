bf7f8622ce34a3f1ce7d8de05beafb526be2edce
Nico Schlömer <nico.schloemer@gmail.com>
2009-08-11 22:24:14 +0000

    created /CMakeModules/FindTrilinos.cmake and have the NOX version compile with it
    git-svn-id: svn://macos.cmi.ua.ac.be:/home/wins/toegw/twna/svnrepos/ginzburg-landau/programs/cpp/trunk@68 1ecbfe60-6773-de11-a22a-003048c688d8

---
+++ CMakeModules/FindTrilinos.cmake
@@ -0,0 +1,66 @@
+# - Try to find Trilinos
+
+# Copyright (c) 2009, Nico Schlömer, <nico.schloemer@ua.ac.be>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+INCLUDE(CheckLibraryExists)
+
+SET(_trilinos_INCLUDE_SEARCH_DIRS
+  C:/trilinos/include
+  C:/trilinos
+  "$ENV{ProgramFiles}/trilinos"
+  /usr/local/trilinos
+  /opt/trilinos
+)
+
+FIND_PATH( Trilinos_INCLUDE_DIR
+           Trilinos_version.h
+           ${_trilinos_INCLUDE_SEARCH_DIRS} )
+
+SET( Trilinos_LIB_DIR ${Trilinos_INCLUDE_DIR}/../lib )
+
+IF (Trilinos_INCLUDE_DIR)
+   SET(Trilinos_FOUND TRUE)
+ELSE (Trilinos_INCLUDE_DIR)
+   SET(Trilinos_FOUND FALSE)
+ENDIF (Trilinos_INCLUDE_DIR)
+
+# handle the QUIETLY and REQUIRED arguments
+IF (Trilinos_FOUND)
+  IF (NOT Trilinos_FIND_QUIETLY)
+    MESSAGE(STATUS "Found Trilinos: ${Trilinos_INCLUDE_DIR}")
+  ENDIF (NOT Trilinos_FIND_QUIETLY)
+
+  # ----------------------------------------------------------------------------
+  FOREACH(COMPONENT ${Trilinos_FIND_COMPONENTS})
+    STRING(TOUPPER ${COMPONENT} UPPERCOMPONENT)
+    STRING(TOLOWER ${COMPONENT} LOWERCOMPONENT)
+
+    # find library
+    FIND_LIBRARY( Trilinos_${UPPERCOMPONENT}_LIBRARY
+                  NAMES ${LOWERCOMPONENT}
+                  HINTS ${Trilinos_LIB_DIR}
+                )
+    IF (NOT Trilinos_${UPPERCOMPONENT}_LIBRARY)
+      MESSAGE(FATAL_ERROR "Could NOT find Trilinos component ${COMPONENT}")
+    ELSE (NOT Trilinos_${UPPERCOMPONENT}_LIBRARY)
+      SET( Trilinos_${UPPERCOMPONENT}_FOUND TRUE )
+      SET( Trilinos_LIBRARIES ${Trilinos_LIBRARIES} ${Trilinos_${UPPERCOMPONENT}_LIBRARY})
+      IF (NOT Trilinos_FIND_QUIETLY)
+        MESSAGE(STATUS "Found Trilinos component: ${COMPONENT}")
+      ENDIF (NOT Trilinos_FIND_QUIETLY)
+    ENDIF(NOT Trilinos_${UPPERCOMPONENT}_LIBRARY)
+
+  ENDFOREACH(COMPONENT)
+  # ----------------------------------------------------------------------------
+
+ELSE (Trilinos_FOUND)
+   IF (Trilinos_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could NOT find Trilinos")
+   ENDIF (Trilinos_FIND_REQUIRED)
+ENDIF (Trilinos_FOUND)
+
+
+MARK_AS_ADVANCED(Trilinos_INCLUDE_DIR)
