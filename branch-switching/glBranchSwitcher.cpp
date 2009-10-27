330a0217a891acb7456965baf818e804a4efbea6
Nico Schlömer <nico.schloemer@gmail.com>
2009-10-27 16:58:30 +0000

    added missing class for branch-switcher
    git-svn-id: svn://macos.cmi.ua.ac.be:/home/wins/toegw/twna/svnrepos/ginzburg-landau/programs/cpp/trunk@138 1ecbfe60-6773-de11-a22a-003048c688d8

---
+++ branch-switching/glBranchSwitcher.cpp
@@ -0,0 +1,218 @@
+#include "glBranchSwitcher.h"
+
+#include "glException.h"
+
+// for the eigenvalue computation:
+#include <AnasaziBasicEigenproblem.hpp>
+#include <AnasaziBasicOutputManager.hpp>
+#include <AnasaziBlockDavidsonSolMgr.hpp>
+#include <AnasaziBlockKrylovSchurSolMgr.hpp>
+#include <AnasaziLOBPCGSolMgr.hpp>
+#include <AnasaziRTRSolMgr.hpp>
+#include <AnasaziBasicEigenproblem.hpp>
+#include <AnasaziConfigDefs.hpp>
+#include <AnasaziEpetraAdapter.hpp>
+#include <AnasaziBasicSort.hpp>
+
+// =============================================================================                       
+GlBranchSwitcher::GlBranchSwitcher ( const Teuchos::RCP<GlSystem> glSystem,
+	                             const double ds,
+                                     const double alpha0,
+                                     const double alpham1,
+                                     const Teuchos::RCP<const Epetra_Vector> u0,
+                                     const Teuchos::RCP<const Epetra_Vector> um1 ) :
+  glSystem_(glSystem),
+  ds_(ds),
+  alpha0_(alpha0),
+  alpham1_(alpham1),
+  u0_( u0 ),
+  um1_( um1 ),
+  dFdalpha_( 0 )
+{
+
+  computePhi2();
+
+}
+// =============================================================================                       
+GlBranchSwitcher::~GlBranchSwitcher()
+{
+}
+// =============================================================================                       
+bool
+GlBranchSwitcher::computeF ( const Epetra_Vector &x,
+                             Epetra_Vector       &F,
+                             const NOX::Epetra::Interface::Required::FillType fillFlag
+         )
+{
+}
+// =============================================================================                       
+bool
+GlBranchSwitcher::computeJacobian ( const Epetra_Vector &x,
+                  Epetra_Operator     &Jac )
+{
+}
+// =============================================================================                       
+bool
+GlBranchSwitcher::computePreconditioner ( const Epetra_Vector     &x,
+                                          Epetra_Operator         &Prec,
+                                          Teuchos::ParameterList* precParams
+                                        )  const
+{
+}
+// =============================================================================                       
+Teuchos::RCP<Epetra_Vector>
+GlBranchSwitcher::getSolution() const
+{
+}
+// =============================================================================                       
+Teuchos::RCP<Epetra_CrsMatrix>
+GlBranchSwitcher::getJacobian() const
+{
+}
+// =============================================================================                       
+void
+GlBranchSwitcher::computePhi2()
+{
+}
+// =============================================================================
+Teuchos::RCP<Epetra_Vector>
+GlBranchSwitcher::getNullvector( const Teuchos::RCP<const Epetra_Operator> A )
+{
+  bool verbose = true;
+  bool debug = true;
+  std::string which ( "SM" ); // smallest magnitude
+
+  bool boolret;
+
+  typedef double ScalarType;
+  typedef Teuchos::ScalarTraits<ScalarType>          SCT;
+  typedef SCT::magnitudeType                         MagnitudeType;
+  typedef Epetra_MultiVector                         MV;
+  typedef Epetra_Operator                            OP;
+  typedef Anasazi::MultiVecTraits<ScalarType,MV>     MVT;
+  typedef Anasazi::OperatorTraits<ScalarType,MV,OP>  OPT;
+
+  Epetra_BlockMap Map = u0_->Map();
+
+  // - - - - - - - - - - - - - - - - -
+  // Start the block Arnoldi iteration
+  // - - - - - - - - - - - - - - - - -
+  // Variables used for the Block Krylov Schur Method
+  int nev = 1; // only get *one* eigenvector here (the one corresponding
+               // to the 0 eigenvalue
+  int blockSize = 10;
+  int numBlocks = 10;
+  int maxRestarts = 25;
+  double tol = 1.0e-10;
+  
+  // Create a sort manager to pass into the block Krylov-Schur solver manager
+  // -->  Make sure the reference-counted pointer is of type Anasazi::SortManager<>
+  // -->  The block Krylov-Schur solver manager uses Anasazi::BasicSort<> by default,
+  //      so you can also pass in the parameter "Which", instead of a sort manager.
+  Teuchos::RCP<Anasazi::SortManager<MagnitudeType> > MySort =
+               Teuchos::rcp ( new Anasazi::BasicSort<MagnitudeType> ( which ) );
+
+  // Set verbosity level
+  int verbosity = Anasazi::Errors + Anasazi::Warnings;
+  verbosity += Anasazi::FinalSummary + Anasazi::TimingDetails;
+  if ( debug )
+  {
+    verbosity += Anasazi::Debug;
+  }
+
+  // Create parameter list to pass into solver manager
+  Teuchos::ParameterList MyPL;
+  MyPL.set ( "Verbosity", verbosity );
+  MyPL.set ( "Sort Manager", MySort );
+  //MyPL.set( "Which", which );
+  MyPL.set ( "Block Size", blockSize );
+  MyPL.set ( "Num Blocks", numBlocks );
+  MyPL.set ( "Maximum Restarts", maxRestarts );
+  //MyPL.set( "Step Size", stepSize );
+  MyPL.set ( "Convergence Tolerance", tol );
+
+  // Create an Epetra_MultiVector for an initial vector to start the solver.
+  // Note:  This needs to have the same number of columns as the blocksize.
+  Teuchos::RCP<Epetra_MultiVector> ivec =
+                     Teuchos::rcp ( new Epetra_MultiVector ( Map, blockSize ) );
+  ivec->Random();
+
+  // Create the eigenproblem.
+  Teuchos::RCP<Anasazi::BasicEigenproblem<double, MV, OP> > MyProblem =
+    Teuchos::rcp ( new Anasazi::BasicEigenproblem<double, MV, OP> ( A, ivec ) );
+
+  // Set the number of eigenvalues requested
+  MyProblem->setNEV ( nev );
+
+  int MyPID = Map.Comm().MyPID();
+
+  // Inform the eigenproblem that you are finishing passing it information
+  bool ierr = MyProblem->setProblem();
+  if ( ierr != true )
+  {
+  if ( MyPID == 0 )
+      cout << "Anasazi::BasicEigenproblem::setProblem() returned with error." << endl;
+#ifdef HAVE_MPI
+      MPI_Finalize() ;
+#endif
+      return Teuchos::ENull();
+  }
+
+  // Initialize the Block Arnoldi solver
+//       Anasazi::BlockDavidsonSolMgr<double, MV, OP> 
+//       Anasazi::LOBPCGSolMgr<double, MV, OP> 
+//       Anasazi::RTRSolMgr<double, MV, OP> 
+   Anasazi::BlockKrylovSchurSolMgr<double, MV, OP> 
+                                                MySolverMgr ( MyProblem, MyPL );
+
+  // Solve the problem to the specified tolerances or length
+  Anasazi::ReturnType returnCode = MySolverMgr.solve();
+  if ( returnCode != Anasazi::Converged && MyPID==0 && verbose )
+       cout << "Anasazi::EigensolverMgr::solve() returned unconverged." << endl;
+
+  // Get the eigenvalues and eigenvectors from the eigenproblem
+  Anasazi::Eigensolution<ScalarType,MV> sol = MyProblem->getSolution();
+  std::vector<Anasazi::Value<ScalarType> > evals = sol.Evals;
+  Teuchos::RCP<MV> evecs = sol.Evecs;
+  std::vector<int> index = sol.index;
+  int numev = sol.numVecs;
+
+  double resTol = 1.0e-14;
+  double realPart = evals[0].realpart;
+  double imagPart = evals[0].imagpart;
+  double abs0 = sqrt( realPart*realPart + imagPart*imagPart );
+  if ( abs0>resTol ) {
+      std::string message = "Computes eigenvalue not 0";
+      throw glException( "GlBranchSwitcher::computeNullvector", message );
+  }
+
+  Teuchos::RCP<Epetra_Vector> ev1 ( ( *evecs ) ( 0 ) );
+
+  return ev1;
+
+}
+// =============================================================================
+void
+GlBranchSwitcher::computedFdalpha()
+{
+
+  LOCA::ParameterVector paramVec;
+  int ierr;
+
+  // set parameter
+  Teuchos::RCP<Epetra_Vector>  FAlpha0 = Teuchos::rcp( new Epetra_Vector(*u0_) );
+  paramVec.setValue ( "H0", alpha0_ );
+  glSystem_->setParameters ( paramVec );
+  ierr = glSystem_->computeF ( *u0_, *FAlpha0 );
+
+  Teuchos::RCP<Epetra_Vector>  dFdalpha = Teuchos::rcp( new Epetra_Vector(*u0_) );
+  paramVec.setValue ( "H0", alpham1_ );
+  glSystem_->setParameters ( paramVec );
+  ierr = glSystem_->computeF ( *um1_, *dFdalpha );
+
+  // compute finite difference
+  double dalpha = alpha0_-alpham1_;
+  dFdalpha->Update( 1.0/dalpha, *FAlpha0, -1.0/dalpha );
+  
+}
+// =============================================================================                       
---
+++ branch-switching/glBranchSwitcher.h
@@ -0,0 +1,105 @@
+#ifndef GLBRANCHSWITCHER_H
+#define GLBRANCHSWITCHER_H
+
+#include <NOX_Epetra_Interface_Jacobian.H>
+
+
+#include "glSystem.h"
+
+#include "ginzburgLandau.h"
+
+#include <Epetra_Comm.h>
+#include <Epetra_Map.h>
+#include <Epetra_Vector.h>
+
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_ENull.hpp>
+
+#include <Epetra_CrsMatrix.h>
+
+#include <Teuchos_ParameterList.hpp>
+
+#include <NOX_Epetra_Interface_Required.H> // NOX base class
+#include <NOX_Epetra_Interface_Jacobian.H> // NOX base class
+
+#include <LOCA_Epetra_Interface_Required.H> // LOCA base class
+
+#include <NOX_Abstract_PrePostOperator.H>
+
+#include <LOCA_Parameter_Vector.H>
+
+#include <Tpetra_Map.hpp>
+#include <Tpetra_Vector.hpp>
+
+#include <NOX_Abstract_Group.H>
+
+
+class GlBranchSwitcher:
+      public NOX::Epetra::Interface::Jacobian
+  {
+  public:
+
+    GlBranchSwitcher ( const Teuchos::RCP<GlSystem> glSystem,
+                       const double ds,
+                       const double alpha0,
+                       const double alpham1,
+                       const Teuchos::RCP<const Epetra_Vector> u0,
+                       const Teuchos::RCP<const Epetra_Vector> um1 );
+                       
+
+    //! Destructor
+    ~GlBranchSwitcher();
+
+    //! Evaluate the Ginzburg--Landau functions at a given state defined
+    //! by the input vector x.
+    virtual bool
+    computeF ( const Epetra_Vector &x,
+               Epetra_Vector       &F,
+               const NOX::Epetra::Interface::Required::FillType fillFlag = NOX::Epetra::Interface::Required::Residual
+             );
+
+    //! Evaluate the Jacobian matrix of the Ginzburg--Landau problem
+    //! at a given state defined by the input vector x.
+    virtual bool
+    computeJacobian ( const Epetra_Vector &x,
+                      Epetra_Operator     &Jac );
+
+    //! Dummy preconditioner function. So far does nothing but throwing
+    //! an exception when called.
+    virtual bool
+    computePreconditioner ( const Epetra_Vector     &x,
+                            Epetra_Operator         &Prec,
+                            Teuchos::ParameterList* precParams=0
+                          )  const;
+
+    //! Returns the current state. Not necessarily a solution to the problem!
+    //! @return Reference-counted pointer to the current state.
+    Teuchos::RCP<Epetra_Vector> getSolution() const;
+
+    //! Returns the current Jacobian.
+    //! @return Reference-counted pointer to the Jacobian.
+    Teuchos::RCP<Epetra_CrsMatrix> getJacobian() const;
+
+  private:
+
+    void
+    computePhi2();
+
+    void
+    computedFdalpha();
+
+    Teuchos::RCP<Epetra_Vector>
+    getNullvector( const Teuchos::RCP<const Epetra_Operator> A );
+
+    const Teuchos::RCP<GlSystem> glSystem_;
+    const Teuchos::RCP<const Tpetra::MultiVector<double,int> > phi2_;
+    const Teuchos::RCP<const Epetra_Vector > u0_;
+    const Teuchos::RCP<const Epetra_Vector > um1_;
+    const Teuchos::RCP<const Epetra_Vector>                    dFdalpha_;
+    const double ds_;
+    const double alpha0_;
+    const double alpham1_;
+
+  };
+
+#endif // GLBRANCHSWITCHER_H
