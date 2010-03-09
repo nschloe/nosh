3c727794f1097a1f1d8a601634f54fecd8a7ebd7
Nico Schlömer <nico.schloemer@gmail.com>
2010-03-09 10:36:53 +0000

    extended unit test framework by proper use of Boost.Test
    git-svn-id: svn://macos.cmi.ua.ac.be:/home/wins/toegw/twna/svnrepos/ginzburg-landau/programs/cpp/trunk@381 1ecbfe60-6773-de11-a22a-003048c688d8

--- packages/libgl/test/main.cpp
+++ packages/libgl/test/main.cpp
@@ -1,3 +1,9 @@
+// see <http://old.nabble.com/Undefined-reference-to-%27main%27-with-Boost-Test.-Why--td15986217.html>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE GRNN MyTest
+
+#include <boost/test/unit_test.hpp>
+
 #include <Teuchos_DefaultComm.hpp>
 
 #ifdef HAVE_MPI
@@ -6,14 +12,12 @@
 #include <Epetra_SerialComm.h>
 #endif
 
-#include <Teuchos_CommandLineProcessor.hpp>
-#include <Teuchos_XMLParameterListHelpers.hpp>
 
-#include <EpetraExt_RowMatrixOut.h>
-
-// =============================================================================
-int main ( int argc, char *argv[] )
-{ 
+BOOST_AUTO_TEST_SUITE( my_suite )
+// =========================================================================
+// <http://www.boost.org/doc/libs/1_42_0/libs/test/doc/html/tutorials/hello-the-testing-world.htmlhh>
+BOOST_AUTO_TEST_CASE( my_test )
+{
     // Initialize MPI
 #ifdef HAVE_MPI
     MPI_Init ( &argc,&argv );
@@ -21,23 +25,53 @@ int main ( int argc, char *argv[] )
 
     // Create a communicator for Tpetra objects
     const Teuchos::RCP<const Teuchos::Comm<int> > Comm =
-    Teuchos::DefaultComm<int>::getComm();
+            Teuchos::DefaultComm<int>::getComm();
 
     // Create a communicator for Epetra objects
 #ifdef HAVE_MPI
     Teuchos::RCP<Epetra_MpiComm> eComm =
-    Teuchos::rcp<Epetra_MpiComm> ( new Epetra_MpiComm ( MPI_COMM_WORLD ) );
+            Teuchos::rcp<Epetra_MpiComm> ( new Epetra_MpiComm ( MPI_COMM_WORLD ) );
 #else
     Teuchos::RCP<Epetra_SerialComm>  eComm =
-    Teuchos::rcp<Epetra_SerialComm> ( new Epetra_SerialComm() );
+            Teuchos::rcp<Epetra_SerialComm> ( new Epetra_SerialComm() );
 #endif
 
-    int status = 0;
+    bool status = true;
 
 #ifdef HAVE_MPI
     MPI_Finalize();
 #endif
 
-    return status;
+    BOOST_CHECK( status );
 }
 // =========================================================================
+BOOST_AUTO_TEST_CASE( my_other_test )
+{
+    // Initialize MPI
+#ifdef HAVE_MPI
+    MPI_Init ( &argc,&argv );
+#endif
+
+    // Create a communicator for Tpetra objects
+    const Teuchos::RCP<const Teuchos::Comm<int> > Comm =
+            Teuchos::DefaultComm<int>::getComm();
+
+    // Create a communicator for Epetra objects
+#ifdef HAVE_MPI
+    Teuchos::RCP<Epetra_MpiComm> eComm =
+            Teuchos::rcp<Epetra_MpiComm> ( new Epetra_MpiComm ( MPI_COMM_WORLD ) );
+#else
+    Teuchos::RCP<Epetra_SerialComm>  eComm =
+            Teuchos::rcp<Epetra_SerialComm> ( new Epetra_SerialComm() );
+#endif
+
+    bool status = true;
+
+#ifdef HAVE_MPI
+    MPI_Finalize();
+#endif
+
+    BOOST_CHECK( status );
+}
+// =========================================================================
+BOOST_AUTO_TEST_SUITE_END()
