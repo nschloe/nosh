3aaebd88d17e63cd00446b50e8f9288f1709967a
Nico Schlömer <nico.schloemer@gmail.com>
2010-05-16 14:49:02 +0000

    improve try-catch-handling in the NOX executable
    git-svn-id: svn://macos.cmi.ua.ac.be:/home/wins/toegw/twna/svnrepos/ginzburg-landau/programs/cpp/trunk@439 1ecbfe60-6773-de11-a22a-003048c688d8

--- executables/nox/main.cpp
+++ executables/nox/main.cpp
@@ -39,217 +39,195 @@ int main ( int argc, char *argv[] )
            Teuchos::rcp<Epetra_SerialComm> ( new Epetra_SerialComm() );
 #endif
 
+    int status = 0;
 
-    // ===========================================================================
-    // handle command line arguments
-    Teuchos::CommandLineProcessor My_CLP;
-
-    My_CLP.setDocString (
-        "This program solves the Ginzburg--Landau problem with a NOX interface.\n"
-    );
-
-    std::string xmlInputFileName = "";
-    My_CLP.setOption ( "xml-input-file", &xmlInputFileName,
-                       "XML file containing the parameter list", true );
-
-    // print warning for unrecognized arguments
-    My_CLP.recogniseAllOptions ( true );
-
-    // do throw exceptions
-    My_CLP.throwExceptions ( true );
-
-    // finally, parse the command line
-    Teuchos::CommandLineProcessor::EParseCommandLineReturn parseReturn;
     try
     {
+        // ===========================================================================
+        // handle command line arguments
+        Teuchos::CommandLineProcessor My_CLP;
+
+        My_CLP.setDocString (
+            "This program solves the Ginzburg--Landau problem with a NOX interface.\n"
+        );
+
+        std::string xmlInputFileName = "";
+        My_CLP.setOption ( "xml-input-file", &xmlInputFileName,
+                          "XML file containing the parameter list", true );
+
+        // print warning for unrecognized arguments
+        My_CLP.recogniseAllOptions ( true );
+
+        // do throw exceptions
+        My_CLP.throwExceptions ( true );
+
+        // finally, parse the command line
+        Teuchos::CommandLineProcessor::EParseCommandLineReturn parseReturn;
         parseReturn = My_CLP.parse ( argc, argv );
-    }
-    catch ( std::exception &e )
-    {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
-    if ( parseReturn == Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED )
-    {
-        return 0;
-    }
-    if ( parseReturn != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL )
-    {
-        return 1; // Error!
-    }
 
-    Teuchos::RCP<Teuchos::ParameterList> paramList =
-        Teuchos::rcp ( new Teuchos::ParameterList );
-    std::cout << "Reading parameter list from \"" << xmlInputFileName << "\"."
-              << std::endl;
-    try
-    {
+        if ( parseReturn == Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED )
+        {
+            return 0;
+        }
+        if ( parseReturn != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL )
+        {
+            return 1; // Error!
+        }
+
+        Teuchos::RCP<Teuchos::ParameterList> paramList =
+            Teuchos::rcp ( new Teuchos::ParameterList );
+        std::cout << "Reading parameter list from \"" << xmlInputFileName << "\"."
+                  << std::endl;
+                  
         Teuchos::updateParametersFromXmlFile ( xmlInputFileName, paramList.get() );
-    }
-    catch ( std::exception &e )
-    {
-        std::cerr << e.what() << "\n"
-                  << "Check your XML file for syntax errors." << std::endl;
-        return 1;
-    }
-    // =========================================================================
-    // extract data of the parameter list
-    Teuchos::ParameterList& ioList = paramList->sublist ( "IO", true );
+        // =========================================================================
+        // extract data of the parameter list
+        Teuchos::ParameterList& ioList = paramList->sublist ( "IO", true );
 
-    boost::filesystem::path xmlPath =
-        boost::filesystem::path ( xmlInputFileName ).branch_path();
+        boost::filesystem::path xmlPath =
+            boost::filesystem::path ( xmlInputFileName ).branch_path();
 
-    boost::filesystem::path inputGuessFile = ioList.get<string> ( "Input guess" );
-    if ( !inputGuessFile.empty() && inputGuessFile.root_directory().empty() ) // if inputGuessFile is not empty and is a relative path
-        inputGuessFile = xmlPath / inputGuessFile;
+        boost::filesystem::path inputGuessFile = ioList.get<string> ( "Input guess" );
+        if ( !inputGuessFile.empty() && inputGuessFile.root_directory().empty() ) // if inputGuessFile is not empty and is a relative path
+            inputGuessFile = xmlPath / inputGuessFile;
 
-    const std::string outputFormat = ioList.get<string> ( "Output format" );
-    
-    // set default directory to be the directory of the XML file itself
-    boost::filesystem::path outputDirectory = ioList.get<string> ( "Output directory" );
-    if ( outputDirectory.root_directory().empty() )
-    {
-        // outputDirectory is empty or is a relative directory.
-        outputDirectory = xmlPath / outputDirectory;
-    }
+        const std::string outputFormat = ioList.get<string> ( "Output format" );
+        
+        // set default directory to be the directory of the XML file itself
+        boost::filesystem::path outputDirectory = ioList.get<string> ( "Output directory" );
+        if ( outputDirectory.root_directory().empty() )
+        {
+            // outputDirectory is empty or is a relative directory.
+            outputDirectory = xmlPath / outputDirectory;
+        }
 
-    bool computeEigenvalues = paramList->sublist ( "Eigenvalues",true )
-                              .get<bool>( "Compute Eigenvalues" );
+        bool computeEigenvalues = paramList->sublist ( "Eigenvalues",true )
+                                  .get<bool>( "Compute Eigenvalues" );
 
-    bool computeConditionNumbers = paramList->sublist ( "Condition Numbers",true )
-                                   .get<bool> ( "Compute Condition Numbers" );
+        bool computeConditionNumbers = paramList->sublist ( "Condition Numbers",true )
+                                      .get<bool> ( "Compute Condition Numbers" );
 
-    bool plotEachNewtonStep = paramList->sublist ( "IO",true )
-                              .get<bool> ( "Plot each Newton step" );
+        bool plotEachNewtonStep = paramList->sublist ( "IO",true )
+                                  .get<bool> ( "Plot each Newton step" );
 
-    std::string jacFilename = paramList->sublist ( "IO",true )
-                              .get<std::string> ( "Jacobian MATLAB matrix file name" );
-    // =========================================================================
+        std::string jacFilename = paramList->sublist ( "IO",true )
+                                  .get<std::string> ( "Jacobian MATLAB matrix file name" );
+        // =========================================================================
 
-    // set problemParameters and glSystem
-    Teuchos::ParameterList                   problemParameters;
-    Teuchos::RCP<Ginla::LocaSystem::Bordered> glSystem = Teuchos::null;
-    Teuchos::RCP<Recti::Grid::Uniform>  grid = Teuchos::null;
-    Teuchos::RCP<ComplexVector> initialPsi = Teuchos::null;
-    if ( !inputGuessFile.empty() )
-    {
-        try
+        // set problemParameters and glSystem
+        Teuchos::ParameterList                    problemParameters;
+        Teuchos::RCP<Ginla::LocaSystem::Bordered> glSystem = Teuchos::null;
+        Teuchos::RCP<Recti::Grid::Uniform>        grid = Teuchos::null;
+        Teuchos::RCP<ComplexVector> initialPsi = Teuchos::null;
+        if ( !inputGuessFile.empty() )
         {
             glNoxHelpers::createGlSystem ( Comm,
-                                           eComm,
-                                           inputGuessFile.string(),
-                                           problemParameters,
-                                           glSystem,
-                                           initialPsi,
-                                           grid );
+                                          eComm,
+                                          inputGuessFile.string(),
+                                          problemParameters,
+                                          glSystem,
+                                          initialPsi,
+                                          grid );
         }
-        catch ( std::exception & e )
+        else
         {
-            std::cerr << e.what() << std::endl;
-            return 1;
+            int    Nx      = paramList->sublist ( "GL",true ).get<int> ( "Nx" );
+            double scaling = paramList->sublist ( "GL",true ).get<double> ( "scaling" );
+            double H0      = paramList->sublist ( "GL",true ).get<double> ( "H0" );
+            paramList->sublist ( "GL",true ).get ( "chi",0.0 );
+            Teuchos::ParameterList & domainParameters =
+                    paramList->sublist ( "Domain",true );
+            glNoxHelpers::createGlSystem ( Comm,
+                                          eComm,
+                                          Nx,
+                                          scaling,
+                                          H0,
+                                          domainParameters,
+                                          problemParameters,
+                                          glSystem,
+                                          initialPsi,
+                                          grid );
         }
-    }
-    else
-    {
-        int    Nx      = paramList->sublist ( "GL",true ).get<int> ( "Nx" );
-        double scaling = paramList->sublist ( "GL",true ).get<double> ( "scaling" );
-        double H0      = paramList->sublist ( "GL",true ).get<double> ( "H0" );
-        paramList->sublist ( "GL",true ).get ( "chi",0.0 );
-        Teuchos::ParameterList & domainParameters =
-                paramList->sublist ( "Domain",true );
-        glNoxHelpers::createGlSystem ( Comm,
-                                       eComm,
-                                       Nx,
-                                       scaling,
-                                       H0,
-                                       domainParameters,
-                                       problemParameters,
-                                       glSystem,
-                                       initialPsi,
-                                       grid );
-    }
 
-    Teuchos::RCP<Teuchos::ParameterList> nlParamsPtr =
-        Teuchos::rcpFromRef ( paramList->sublist ( "NOX",true ) );
+        Teuchos::RCP<Teuchos::ParameterList> nlParamsPtr =
+            Teuchos::rcpFromRef ( paramList->sublist ( "NOX",true ) );
 
-    if ( plotEachNewtonStep )
-    {
-        const std::string fileBaseName = "newtonStep";
-        const std::string outputFormat = "VTI";
-        const unsigned int maxIndex    = 1000; // TODO replace by maxnumsteps
-        Teuchos::RCP<Ginla::IO::StateWriter> stateWriter =
-            Teuchos::rcp( new Ginla::IO::StateWriter( outputDirectory.string(),
-                                                      fileBaseName,
-                                                      outputFormat,
-                                                      maxIndex ) );
-                     
-        Teuchos::RCP<NOX::Abstract::PrePostOperator> ppo =
-            Teuchos::rcp ( new Ginla::IO::SaveNewtonData ( stateWriter,
-                                                           grid,
-                                                           glSystem->getKomplex() ) );
-        nlParamsPtr->sublist ( "Solver Options" )
-                    .set ( "User Defined Pre/Post Operator", ppo );
-    }
+        if ( plotEachNewtonStep )
+        {
+            const std::string fileBaseName = "newtonStep";
+            const std::string outputFormat = "VTI";
+            const unsigned int maxIndex    = 1000; // TODO replace by maxnumsteps
+            Teuchos::RCP<Ginla::IO::StateWriter> stateWriter =
+                Teuchos::rcp( new Ginla::IO::StateWriter( outputDirectory.string(),
+                                                          fileBaseName,
+                                                          outputFormat,
+                                                          maxIndex ) );
+                        
+            Teuchos::RCP<NOX::Abstract::PrePostOperator> ppo =
+                Teuchos::rcp ( new Ginla::IO::SaveNewtonData ( stateWriter,
+                                                              grid,
+                                                              glSystem->getKomplex() ) );
+            nlParamsPtr->sublist ( "Solver Options" )
+                        .set ( "User Defined Pre/Post Operator", ppo );
+        }
 
-    // create NOX group
-    Teuchos::RCP<NOX::Epetra::Group> grpPtr =
-        glNoxHelpers::createSolverGroup ( glSystem,
-                                          nlParamsPtr,
-                                          initialPsi
-                                        );
+        // create NOX group
+        Teuchos::RCP<NOX::Epetra::Group> grpPtr =
+            glNoxHelpers::createSolverGroup ( glSystem,
+                                              nlParamsPtr,
+                                              initialPsi
+                                            );
 
-    // create convergence test from sublist
-    Teuchos::RCP<NOX::StatusTest::Generic> statusTest =
-        glNoxHelpers::createConvergenceTest ( paramList->sublist ( "NOX Status Test",true ),
-                                              *nlParamsPtr );
+        // create convergence test from sublist
+        Teuchos::RCP<NOX::StatusTest::Generic> statusTest =
+            glNoxHelpers::createConvergenceTest ( paramList->sublist ( "NOX Status Test",true ),
+                                                  *nlParamsPtr );
 
-    // create solver object
-    Teuchos::RCP<NOX::Solver::Generic> solver =
-        glNoxHelpers::createSolver ( grpPtr, statusTest, nlParamsPtr );
+        // create solver object
+        Teuchos::RCP<NOX::Solver::Generic> solver =
+            glNoxHelpers::createSolver ( grpPtr, statusTest, nlParamsPtr );
 
-    // solve the system
-    try
-    {
+        // solve the system
         NOX::StatusTest::StatusType solvStatus = solver->solve();
+
+        // compute the condition number
+        if ( computeConditionNumbers )
+        {
+            double kappa = glNoxHelpers::computeJacobianConditionNumber ( solver, grpPtr );
+            std::cout << "Condition number: kappa = " << kappa << "." << std::endl;
+        }
+
+        // spit out the Jacobian in MATLAB readable format
+        if ( !jacFilename.empty() )
+        {
+    //       EpetraExt::RowMatrixToMatlabFile(jacFilename.c_str(),*(glsystem->getJacobian()));
+        }
+
+        // compute the eigenvalues of the Jacobian
+        if ( computeEigenvalues )
+            glNoxHelpers::computeJacobianEigenvalues ( solver, grpPtr, Comm->getRank() );
+
+        // print the solution to a file
+        std::string solFileBasename = ( outputDirectory / "solution" ).string();
+        
+        glNoxHelpers::printSolutionToFile ( solver,
+                                            glSystem,
+                                            solFileBasename );
+
+        // check the convergence status
+        int status = glNoxHelpers::checkConvergence ( solver );
+        
     }
     catch ( std::exception & e )
     {
         std::cerr << e.what() << std::endl;
-        return 1;
+        status += 10;
     }
-    catch ( int e )
+    catch (...)
     {
-        std::cerr << "Solver returned error code " << e << "." << std::endl;
-        return 1;
+        std::cerr << "Caught unknown exception." << std::endl;
+        status += 10;
     }
-
-
-    // compute the condition number
-    if ( computeConditionNumbers )
-    {
-        double kappa = glNoxHelpers::computeJacobianConditionNumber ( solver, grpPtr );
-        std::cout << "Condition number: kappa = " << kappa << "." << std::endl;
-    }
-
-    // spit out the Jacobian in MATLAB readable format
-    if ( !jacFilename.empty() )
-    {
-//       EpetraExt::RowMatrixToMatlabFile(jacFilename.c_str(),*(glsystem->getJacobian()));
-    }
-
-    // compute the eigenvalues of the Jacobian
-    if ( computeEigenvalues )
-        glNoxHelpers::computeJacobianEigenvalues ( solver, grpPtr, Comm->getRank() );
-
-    // print the solution to a file
-    std::string solFileBasename = ( outputDirectory / "solution" ).string();
-    
-    glNoxHelpers::printSolutionToFile ( solver,
-                                        glSystem,
-                                        solFileBasename );
-
-    // check the convergence status
-    int status = glNoxHelpers::checkConvergence ( solver );
 
 #ifdef HAVE_MPI
     MPI_Finalize();
