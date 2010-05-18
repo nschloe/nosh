5e5a06a2705182d69611e4cc3901f11f482559b9
Nico Schlömer <nico.schloemer@gmail.com>
2010-05-18 09:48:48 +0000

    added missing test files
    git-svn-id: svn://macos.cmi.ua.ac.be:/home/wins/toegw/twna/svnrepos/ginzburg-landau/programs/cpp/trunk@448 1ecbfe60-6773-de11-a22a-003048c688d8

---
+++ packages/ginla/test/fiveStep/main.cpp
@@ -0,0 +1,351 @@
+// see <http://old.nabble.com/Undefined-reference-to-%27main%27-with-Boost-Test.-Why--td15986217.html>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE GRNN zeroStepLocaTest
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#include <Teuchos_DefaultComm.hpp>
+#include <Teuchos_CommandLineProcessor.hpp>
+#include <Teuchos_ParameterList.hpp>
+#include <Teuchos_XMLParameterListHelpers.hpp>
+
+#include <Epetra_CrsMatrix.h>
+
+#include <LOCA_Epetra_Factory.H>
+#include <LOCA_Epetra_Group.H>
+#include <LOCA_StatusTest_MaxIters.H>
+#include <NOX_Epetra_LinearSystem_AztecOO.H>
+#include <NOX_StatusTest_NormF.H>
+#include <NOX_StatusTest_MaxIters.H>
+#include <NOX_StatusTest_Combo.H>
+// #include <NOX_Epetra_Group.H>
+
+#include "Ginla_MagneticVectorPotential_Centered.h"
+#include "Ginla_Operator_BCCentral.h"
+#include "Ginla_IO_StatsWriter.h"
+#include "Ginla_IO_StateWriter.h"
+#include "Ginla_IO_SaveEigenData.h"
+#include "Ginla_Helpers.h"
+#include "Ginla_LocaSystem_Bordered.h"
+
+#include "Recti_Grid_Uniform.h"
+#include "Recti_Grid_Reader.h"
+
+#ifdef HAVE_MPI
+#include <Epetra_MpiComm.h>
+#else
+#include <Epetra_SerialComm.h>
+#endif
+
+#include <boost/filesystem.hpp>
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// ===========================================================================
+// <http://www.boost.org/doc/libs/1_42_0/libs/test/doc/html/tutorials/hello-the-testing-world.htmlhh>
+BOOST_AUTO_TEST_CASE( zero_step_loca_test )
+{
+    // Initialize MPI
+#ifdef HAVE_MPI
+    MPI_Init ( NULL, NULL );
+#endif
+
+    // Create a communicator for Tpetra objects
+    const Teuchos::RCP<const Teuchos::Comm<int> > Comm =
+            Teuchos::DefaultComm<int>::getComm();
+
+    // Create a communicator for Epetra objects
+#ifdef HAVE_MPI
+    Teuchos::RCP<Epetra_MpiComm> eComm =
+            Teuchos::rcp<Epetra_MpiComm> ( new Epetra_MpiComm ( MPI_COMM_WORLD ) );
+#else
+    Teuchos::RCP<Epetra_SerialComm>  eComm =
+            Teuchos::rcp<Epetra_SerialComm> ( new Epetra_SerialComm() );
+#endif
+
+    // ------------------------------------------------------------------------
+    // handle command line arguments
+    Teuchos::CommandLineProcessor My_CLP;
+
+    std::string xmlInputFileName = "";
+    My_CLP.setOption ( "xml-input-file", &xmlInputFileName,
+                       "XML file containing the parameter list", true );
+                       
+    std::string expSolFileName = "";
+    My_CLP.setOption ( "expected-solution-file", &expSolFileName,
+                       "VTK/VTI file containing the expected solution", true );
+
+    // print warning for unrecognized arguments
+    My_CLP.recogniseAllOptions ( true );
+
+    // don't throw exceptions
+    My_CLP.throwExceptions ( true );
+
+    // finally, parse the stuff!
+    Teuchos::CommandLineProcessor::EParseCommandLineReturn parseReturn;
+    
+    parseReturn = My_CLP.parse ( boost::unit_test::framework::master_test_suite().argc,
+                                 boost::unit_test::framework::master_test_suite().argv );
+    // ------------------------------------------------------------------------
+    // Read the XML file
+    Teuchos::RCP<Teuchos::ParameterList> paramList =
+            Teuchos::rcp ( new Teuchos::ParameterList );
+
+    Teuchos::updateParametersFromXmlFile ( xmlInputFileName,
+                                           paramList.get()
+                                         );
+    // ------------------------------------------------------------------------
+    // extract data for the output the parameter list
+    Teuchos::ParameterList outputList;
+    outputList = paramList->sublist ( "Output", true );
+
+    // set default directory to be the directory of the XML file itself
+    std::string xmlPath = boost::filesystem::path ( xmlInputFileName ).branch_path().string();
+    boost::filesystem::path outputDirectory = outputList.get<string> ( "Output directory" );
+    if ( outputDirectory.root_directory().empty() ) // outputDirectory is empty or is a relative directory.
+        outputDirectory = xmlPath / outputDirectory;
+    std::string contFileBaseName =
+        outputList.get<string> ( "Continuation file base name" );
+    std::string outputFormat =
+        outputList.get<string> ( "Output format" );
+    std::string contDataFileName =
+        outputList.get<string> ( "Continuation data file name" );
+    // ------------------------------------------------------------------------
+    Teuchos::ParameterList glParameters;
+    Teuchos::RCP<ComplexVector> psi;
+    Teuchos::RCP<Recti::Grid::Uniform> grid;
+
+    Teuchos::ParameterList initialGuessList;
+    initialGuessList = paramList->sublist ( "Initial guess", true );
+
+    boost::filesystem::path inputGuessFile = initialGuessList.get<string> ( "File name" );
+    if ( !inputGuessFile.empty() && inputGuessFile.root_directory().empty() ) // if inputGuessFile is a relative path
+        inputGuessFile = xmlPath / inputGuessFile;
+
+    BOOST_REQUIRE( !inputGuessFile.empty() );
+    
+    // For technical reasons, the reader can only accept ComplexMultiVectors.
+    Teuchos::RCP<ComplexMultiVector> psiM;
+    Recti::Grid::Reader::read ( Comm, inputGuessFile.string(), psiM, grid, glParameters );
+    TEUCHOS_ASSERT_EQUALITY ( psiM->getNumVectors(), 1 );
+    psi = psiM->getVectorNonConst ( 0 );
+
+    // possibly overwrite the parameters
+    Teuchos::ParameterList & overwriteParamsList = paramList->sublist ( "Overwrite parameter list", true ); 
+    bool overwriteParameters = overwriteParamsList.get<bool> ( "Overwrite parameters" );
+    if ( overwriteParameters )
+    {
+        Teuchos::ParameterList & overwritePList = overwriteParamsList.sublist( "Parameters", true );
+        glParameters.setParameters( overwritePList );
+        
+        // possibly update the scaling of the grid
+        grid->updateScaling( glParameters.get<double>("scaling") );
+    }
+    // ------------------------------------------------------------------------
+
+    Teuchos::RCP<Ginla::MagneticVectorPotential::Centered> A =
+        Teuchos::rcp ( new Ginla::MagneticVectorPotential::Centered ( glParameters.get<double> ( "H0" ),
+                                                                      glParameters.get<double> ( "scaling" ) ) );
+
+    // create the operator
+    Teuchos::RCP<Ginla::Operator::Virtual> glOperator =
+        Teuchos::rcp ( new Ginla::Operator::BCCentral ( grid, A, psi->getMap(), psi->getMap() ) );
+
+    std::string fn = outputDirectory.string() + "/" + contDataFileName;
+    Teuchos::RCP<Ginla::IO::StatsWriter> statsWriter = 
+            Teuchos::rcp( new Ginla::IO::StatsWriter( fn ) );
+
+    Teuchos::ParameterList & stepperList = paramList->sublist ( "LOCA" ).sublist ( "Stepper" );
+    int maxLocaSteps = stepperList.get<int> ( "Max Steps" );
+        
+    Teuchos::RCP<Ginla::IO::StateWriter> stateWriter =
+        Teuchos::rcp( new Ginla::IO::StateWriter( outputDirectory.string(),
+                                                  contFileBaseName,
+                                                  outputFormat,
+                                                  maxLocaSteps ) );
+    
+    Teuchos::RCP<Ginla::LocaSystem::Bordered> glsystem =
+               Teuchos::rcp ( new Ginla::LocaSystem::Bordered ( glOperator,
+                                                                eComm,
+                                                                psi->getMap(),
+                                                                statsWriter,
+                                                                stateWriter ) );
+    
+    // set the initial value from glParameters
+    std::string contParam = stepperList.get<string> ( "Continuation Parameter" );
+
+    BOOST_REQUIRE( glParameters.isParameter ( contParam ) );
+
+    // check if the initial value was given (will be unused anyway)
+    if ( stepperList.isParameter ( "Initial Value" ) )
+    {
+        std::cerr << "Warning: Parameter 'LOCA->Stepper->Initial Value' given, but will not be used."
+                  << std::endl;
+    }
+
+    stepperList.set ( "Initial Value", glParameters.get<double> ( contParam ) );
+
+    // ------------------------------------------------------------------------
+    // Create the necessary objects
+    // ------------------------------------------------------------------------
+    // Create Epetra factory
+    Teuchos::RCP<LOCA::Abstract::Factory> epetraFactory =
+        Teuchos::rcp ( new LOCA::Epetra::Factory );
+
+    // Create global data object
+    Teuchos::RCP<LOCA::GlobalData> globalData =
+        LOCA::createGlobalData ( paramList, epetraFactory );
+    // ------------------------------------------------------------------------
+#ifdef HAVE_LOCA_ANASAZI
+    Teuchos::ParameterList& eigenList =
+        paramList->sublist ( "LOCA" ).sublist ( "Stepper" ) .sublist ( "Eigensolver" );
+    Teuchos::RCP<Teuchos::ParameterList> eigenListPtr =
+        Teuchos::rcpFromRef ( ( eigenList ) );
+    std::string eigenvaluesFileName =
+        outputList.get<string> ( "Eigenvalues file name" );
+    std::string eigenstateFileNameAppendix =
+        outputList.get<string> ( "Eigenstate file name appendix" );
+
+    Teuchos::RCP<Ginla::IO::StatsWriter> eigenStatsWriter =
+        Teuchos::rcp( new Ginla::IO::StatsWriter( eigenvaluesFileName ) );
+
+    Teuchos::RCP<Ginla::IO::SaveEigenData> glEigenSaver =    
+        Teuchos::RCP<Ginla::IO::SaveEigenData> ( new Ginla::IO::SaveEigenData ( eigenListPtr,
+                                                                                glsystem,
+                                                                                eigenStatsWriter ) );
+
+    Teuchos::RCP<LOCA::SaveEigenData::AbstractStrategy> glSaveEigenDataStrategy = glEigenSaver;
+    eigenList.set ( "glSaveEigenDataStrategy", glSaveEigenDataStrategy );
+#endif
+
+    // ------------------------------------------------------------------------
+    // Create all possible Epetra_Operators.
+    Teuchos::RCP<Epetra_RowMatrix> J = glsystem->getJacobian();
+//  Teuchos::RCP<Epetra_RowMatrix> M = glsystem->getPreconditioner();
+
+    // Create the linear system.
+    // Use the TimeDependent interface for computation of shifted matrices.
+    Teuchos::RCP<LOCA::Epetra::Interface::Required> iReq = glsystem;
+    Teuchos::RCP<NOX::Epetra::Interface::Jacobian> iJac = glsystem;
+//  Teuchos::RCP<NOX::Epetra::Interface::Preconditioner> iPrec = glsystem;
+
+    Teuchos::ParameterList& nlPrintParams =
+        paramList->sublist ( "NOX" )
+                  .sublist ( "Printing" );
+
+    Teuchos::ParameterList& lsParams =
+        paramList->sublist ( "NOX" )
+                  .sublist ( "Direction" )
+                  .sublist ( "Newton" )
+                  .sublist ( "Linear Solver" );
+
+    NOX::Epetra::Vector cloneVector( Epetra_Vector( *glsystem->getExtendedMap() ) );
+    Teuchos::RCP<NOX::Epetra::LinearSystemAztecOO> linSys =
+        Teuchos::rcp ( new NOX::Epetra::LinearSystemAztecOO ( nlPrintParams,
+                                                              lsParams,
+                                                              iReq,
+                                                              iJac,
+                                                              J,
+                                                              cloneVector ) );
+
+    Teuchos::RCP<LOCA::Epetra::Interface::TimeDependent> iTime = glsystem;
+    // ------------------------------------------------------------------------
+    // Create a group which uses that problem interface. The group will
+    // be initialized to contain the default initial guess for the
+    // specified problem.
+
+    // translate parameters into a LOCA list
+    LOCA::ParameterVector locaParams =
+          *(Ginla::Helpers::teuchosParameterList2locaParameterVector( glParameters ));
+
+    // get initial guess
+    double chi = 0.0;
+    NOX::Epetra::Vector initialGuess ( glsystem->createInterfaceState( psi, chi ),
+                                       NOX::Epetra::Vector::CreateView
+                                     );
+
+    Teuchos::RCP<LOCA::Epetra::Group> grp =
+        Teuchos::rcp ( new LOCA::Epetra::Group ( globalData,
+                                                 nlPrintParams,
+                                                 iTime,
+                                                 initialGuess,
+                                                 linSys,
+                                                 linSys,
+                                                 locaParams ) );
+
+    grp->setParams ( locaParams );
+    // ------------------------------------------------------------------------
+    // Set up the NOX status tests
+    Teuchos::ParameterList& noxList = paramList->sublist ( "NOX", true );
+    double tol = noxList.get<double> ( "Tolerance" );
+    int maxNonlinarSteps = noxList.get<int> ( "Max steps" );
+    Teuchos::RCP<NOX::StatusTest::NormF> normF =
+        Teuchos::rcp ( new NOX::StatusTest::NormF ( tol ) );
+    Teuchos::RCP<NOX::StatusTest::MaxIters> maxIters =
+        Teuchos::rcp ( new NOX::StatusTest::MaxIters ( maxNonlinarSteps ) );
+    Teuchos::RCP<NOX::StatusTest::Generic> comboOR =
+        Teuchos::rcp ( new NOX::StatusTest::Combo ( NOX::StatusTest::Combo::OR,
+                                                    normF,
+                                                    maxIters ) );
+    // ------------------------------------------------------------------------
+    // Set up the LOCA status tests
+    Teuchos::RCP<LOCA::StatusTest::MaxIters> maxLocaStepsTest;
+    
+    maxLocaStepsTest = Teuchos::rcp ( new LOCA::StatusTest::MaxIters ( maxLocaSteps ) );
+    // ------------------------------------------------------------------------
+    // Create the stepper
+    Teuchos::RCP<LOCA::Stepper> stepper =
+        Teuchos::rcp ( new LOCA::Stepper ( globalData, grp, maxLocaStepsTest, comboOR, paramList ) );
+    // ------------------------------------------------------------------------
+    // make sure that the stepper starts off with the correct starting value
+    // pass pointer to stepper to glsystem to be able to read stats from the stepper in there
+    glsystem->setLocaStepper ( stepper );
+#ifdef HAVE_LOCA_ANASAZI
+    glEigenSaver->setLocaStepper ( stepper );
+#endif
+    // ------------------------------------------------------------------------
+    // Perform continuation run
+    LOCA::Abstract::Iterator::IteratorStatus status;
+    status = stepper->run();
+    // ------------------------------------------------------------------------
+    // retrieve solution
+//     const NOX::Epetra::Group & finalGroup =
+//         dynamic_cast<const NOX::Epetra::Group&> ( stepper->getSolutionGroup() );
+// 
+//     const Epetra_Vector & finalSolution =
+//         ( dynamic_cast<const NOX::Epetra::Vector&> ( finalGroup.getX() ) ).getEpetraVector();
+    // ------------------------------------------------------------------------
+    // read expected solution from file
+    // For technical reasons, the reader can only accept ComplexMultiVectors.
+    Teuchos::RCP<ComplexMultiVector> psiMSol;
+    Recti::Grid::Reader::read ( Comm, expSolFileName, psiMSol, grid, glParameters );
+    TEUCHOS_ASSERT_EQUALITY ( psiM->getNumVectors(), 1 );
+    
+    Teuchos::RCP<ComplexVector> psiRefSol = psiM->getVectorNonConst ( 0 );
+    // ------------------------------------------------------------------------
+    // compare the results:
+    // get final solution
+    Teuchos::RCP<ComplexVector> diff =  Teuchos::rcp( new ComplexVector( *psi ) );
+
+    diff->update( -1.0, *psiRefSol, 1.0 );
+    
+    Teuchos::Tuple<double,1> nrm;
+    diff->normInf( nrm );
+    
+    BOOST_CHECK_SMALL( nrm[0], 1.0e-12 );
+    // ------------------------------------------------------------------------
+    // clean up
+    LOCA::destroyGlobalData ( globalData );
+    glsystem->releaseLocaStepper();
+#ifdef HAVE_LOCA_ANASAZI
+    glEigenSaver->releaseLocaStepper();
+#endif
+#ifdef HAVE_MPI
+    MPI_Finalize();
+#endif
+    // ------------------------------------------------------------------------
+
+    return;
+}
+// ============================================================================
---
+++ packages/ginla/test/operator/main.cpp
@@ -0,0 +1,111 @@
+// see <http://old.nabble.com/Undefined-reference-to-%27main%27-with-Boost-Test.-Why--td15986217.html>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE GRNN operatorTest
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#include <Teuchos_DefaultComm.hpp>
+
+#include "Ginla_State.h"
+#include "Ginla_Operator_BCCentral.h"
+#include "Ginla_MagneticVectorPotential_Centered.h"
+
+#include "Recti_Domain_Circle.h"
+#include "Recti_Domain_Square.h"
+
+#include "Recti_Grid_Uniform.h"
+#include "Recti_Grid_Reader.h"
+
+#ifdef HAVE_MPI
+#include <Epetra_MpiComm.h>
+#else
+#include <Epetra_SerialComm.h>
+#endif
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// ===========================================================================
+// <http://www.boost.org/doc/libs/1_42_0/libs/test/doc/html/tutorials/hello-the-testing-world.htmlhh>
+BOOST_AUTO_TEST_CASE( operator_test )
+{
+    // Initialize MPI
+#ifdef HAVE_MPI
+    MPI_Init ( NULL, NULL );
+#endif
+
+    // Create a communicator for Tpetra objects
+    const Teuchos::RCP<const Teuchos::Comm<int> > Comm =
+            Teuchos::DefaultComm<int>::getComm();
+
+    // Create a communicator for Epetra objects
+#ifdef HAVE_MPI
+    Teuchos::RCP<Epetra_MpiComm> eComm =
+            Teuchos::rcp<Epetra_MpiComm> ( new Epetra_MpiComm ( MPI_COMM_WORLD ) );
+#else
+    Teuchos::RCP<Epetra_SerialComm>  eComm =
+            Teuchos::rcp<Epetra_SerialComm> ( new Epetra_SerialComm() );
+#endif
+
+    // ------------------------------------------------------------------------
+    // Create grid that contains all node types.
+    // Let Omega be the unit circle.
+    Teuchos::RCP<Recti::Domain::Abstract> Omega
+         = Teuchos::rcp( new Recti::Domain::Circle(1.0) );
+
+    double h = 5.0e-2;
+    
+    Teuchos::RCP<Recti::Grid::Uniform> grid
+         = Teuchos::rcp( new Recti::Grid::Uniform( Omega, h ) );
+    // ------------------------------------------------------------------------
+    // create state on the grid
+    Teuchos::RCP<Ginla::State> state
+        = Teuchos::rcp( new Ginla::State( Comm, grid ) );
+    state->getValuesNonConst()->putScalar( 1.0 );
+    // ------------------------------------------------------------------------
+    // create operator
+    double h0 = 1.0;
+    double edgeLength = 1.0;
+    Teuchos::RCP<Ginla::MagneticVectorPotential::Centered> A
+        = Teuchos::rcp( new Ginla::MagneticVectorPotential::Centered( h0,
+                                                                      edgeLength ) );
+                                                                      
+    Teuchos::RCP<Ginla::Operator::BCCentral> glOperator
+        = Teuchos::rcp( new Ginla::Operator::BCCentral( grid,
+                                                        A,
+                                                        state->getValuesConst()->getMap(),
+                                                        state->getValuesConst()->getMap() ) );
+    // ------------------------------------------------------------------------
+    // get dF/dh0 at state
+    Teuchos::RCP<Ginla::State> dFState = glOperator->getDFDh0( state );
+    // ------------------------------------------------------------------------
+    // compute the finite difference
+    double eps = 1.0e-05;
+    LOCA::ParameterVector p;
+    p.addParameter( "H0", h0 );
+    
+    p[0] = h0 + eps;
+    glOperator->setParameters( p );
+    Teuchos::RCP<Ginla::State> statePlus = glOperator->getF( state );
+    
+    p[0] = h0 - eps;
+    glOperator->setParameters( p );
+    Teuchos::RCP<Ginla::State> stateMinus = glOperator->getF( state );
+    
+    // store the finite difference in statePlus
+    statePlus->update( -0.5/eps, *stateMinus, 0.5/eps );
+    // ------------------------------------------------------------------------
+    // check that the finite difference is somewhere near the analytic expression
+    statePlus->update( -1.0, *dFState, 1.0 );
+    
+    BOOST_CHECK_SMALL( statePlus->normalizedScaledL2Norm(), 1.0e-05 );
+    // ------------------------------------------------------------------------
+    // clean up
+#ifdef HAVE_MPI
+    MPI_Finalize();
+#endif
+    // ------------------------------------------------------------------------
+
+    return;
+}
+// ============================================================================
