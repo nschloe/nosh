2d3985d0b63ea55a440a3548ef30901227655f1c
Nico Schlömer <nico.schloemer@gmail.com>
2010-10-06 19:05:56 +0000

    added missing vector potential files
    git-svn-id: svn://macos.cmi.ua.ac.be:/home/wins/toegw/twna/svnrepos/ginzburg-landau/programs/cpp/trunk@534 1ecbfe60-6773-de11-a22a-003048c688d8

---
+++ packages/ginla/src/Ginla_MagneticVectorPotential_Y.cpp
@@ -0,0 +1,104 @@
+#include "Ginla_MagneticVectorPotential_Y.h"
+
+// ============================================================================
+Ginla::MagneticVectorPotential::Y::
+Y( double mu ) :
+  Ginla::MagneticVectorPotential::Virtual( mu )
+{
+}
+// ============================================================================
+Ginla::MagneticVectorPotential::Y::
+~Y()
+{
+}
+// ============================================================================
+bool
+Ginla::MagneticVectorPotential::Y::
+setParameters( const LOCA::ParameterVector & p )
+{
+    bool valuesChanged = false;
+  
+    if (p.isParameter( "H0" ))
+        if ( mu_ != p.getValue ( "H0" ) )
+        {
+            mu_ = p.getValue ( "H0" );
+            valuesChanged = true;
+        }
+
+    return valuesChanged;
+}
+// ============================================================================
+Teuchos::RCP<LOCA::ParameterVector>
+Ginla::MagneticVectorPotential::Y::
+getParameters() const
+{
+  Teuchos::RCP<LOCA::ParameterVector> p =
+          Teuchos::rcp( new LOCA::ParameterVector() );
+          
+  p->addParameter( "H0", mu_ );
+          
+  return p;
+}
+// ============================================================================
+Teuchos::RCP<Point>
+Ginla::MagneticVectorPotential::Y::
+getA(const Point & x) const
+{
+  return Teuchos::rcp( new Point( Teuchos::tuple<double>( this->getAx(x),
+                                                          this->getAy(x),
+                                                          this->getAz(x)
+                     ) ) );
+}
+// ============================================================================
+double
+Ginla::MagneticVectorPotential::Y::
+getAx(const Point & x) const
+{
+  return   mu_ * 0.5*x[2];
+}
+// ============================================================================
+double
+Ginla::MagneticVectorPotential::Y::
+getAy(const Point & x) const
+{
+  return 0.0;
+}
+// ============================================================================
+double
+Ginla::MagneticVectorPotential::Y::
+getAz(const Point & x) const
+{
+  return - mu_ * 0.5*x[0];
+}
+// ============================================================================
+Teuchos::RCP<Point>
+Ginla::MagneticVectorPotential::Y::
+getDADMu(const Point & x) const
+{
+  return Teuchos::rcp( new Point( Teuchos::tuple<double>( this->getDAxDMu(x),
+                                                          this->getDAyDMu(x),
+                                                          this->getDAzDMu(x)
+                     ) ) );
+}
+// ============================================================================
+double
+Ginla::MagneticVectorPotential::Y::
+getDAxDMu(const Point & x) const
+{
+  return 0.5*x[2];
+}
+// ============================================================================
+double
+Ginla::MagneticVectorPotential::Y::
+getDAyDMu(const Point & x) const
+{
+  return 0.0;
+}
+// ============================================================================
+double
+Ginla::MagneticVectorPotential::Y::
+getDAzDMu(const Point & x) const
+{
+  return - 0.5*x[0];
+}
+// ============================================================================
