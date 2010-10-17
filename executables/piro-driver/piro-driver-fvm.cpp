921c5efb8a647e3f26f0fd29f4d76157d852262e
Nico Schlömer <nico.schloemer@gmail.com>
2010-10-17 17:24:23 +0000

    removed calls to exit()
    git-svn-id: svn://macos.cmi.ua.ac.be:/home/wins/toegw/twna/svnrepos/ginzburg-landau/programs/cpp/trunk@553 1ecbfe60-6773-de11-a22a-003048c688d8

--- executables/piro-driver/piro-driver-fvm.cpp
+++ executables/piro-driver/piro-driver-fvm.cpp
@@ -451,6 +451,6 @@ int main ( int argc, char *argv[] )
       MPI_Finalize();
 #endif
 
-    exit( status==0 ? EXIT_SUCCESS : EXIT_FAILURE );
+    return status==0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 // =========================================================================
