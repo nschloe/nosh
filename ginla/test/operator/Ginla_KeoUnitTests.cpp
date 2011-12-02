cb276a7058f5f86bff1d4d1e7b05ad0178418d94
Nico Schlömer <nico.schloemer@gmail.com>
2011-12-02 01:21:07 -0500

    separated out testKeo method and added a large-cube test case

--- ginla/test/operator/Ginla_KeoUnitTests.cpp
+++ ginla/test/operator/Ginla_KeoUnitTests.cpp
@@ -1,3 +1,22 @@
+// @HEADER
+//
+//    <one line to give the program's name and a brief idea of what it does.>
+//    Copyright (C) 2011  Nico Schl\"omer
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @HEADER
 #include <Teuchos_ParameterList.hpp>
 
 #ifdef HAVE_MPI
@@ -18,8 +37,16 @@
 
 namespace {
 
-// ===========================================================================
-TEUCHOS_UNIT_TEST( Ginla, KeoHashes )
+// =============================================================================
+void
+testKeo( const std::string & inputFileNameBase,
+         const double mu,
+         const double controlNormOne,
+         const double controlNormInf,
+         const double controlSum,
+         const double controlSumReal,
+         Teuchos::FancyOStream & out,
+         bool & success )
 {
     // Create a communicator for Epetra objects
 #ifdef HAVE_MPI
@@ -30,11 +57,11 @@ TEUCHOS_UNIT_TEST( Ginla, KeoHashes )
             Teuchos::rcp<Epetra_SerialComm> ( new Epetra_SerialComm() );
 #endif
 
-    std::string inputFileName( "" );
+    std::string inputFileName;
     if ( eComm->NumProc() == 1 )
-        inputFileName = "cubesmall.e";
+        inputFileName = inputFileNameBase + ".e";
     else
-        inputFileName = "cubesmall-balanced.par";
+        inputFileName = inputFileNameBase + "-balanced.par";
     // =========================================================================
     // Read the data from the file.
     Teuchos::ParameterList data;
@@ -48,7 +75,6 @@ TEUCHOS_UNIT_TEST( Ginla, KeoHashes )
     Teuchos::ParameterList           & problemParameters = data.get( "Problem parameters", Teuchos::ParameterList() );
 
     Teuchos::RCP<Ginla::MagneticVectorPotential> mvp;
-    double mu = 1.0e-2;
     mvp = Teuchos::rcp ( new Ginla::MagneticVectorPotential ( mesh, mvpValues, mu ) );
 
     Teuchos::RCP<LOCA::ParameterVector> mvpParameters =
@@ -68,10 +94,6 @@ TEUCHOS_UNIT_TEST( Ginla, KeoHashes )
     double normOne = keoMatrix->NormOne();
     double normInf = keoMatrix->NormInf();
 
-    // control values
-    double controlNormOne = 10.1456474156918;
-    double controlNormInf = 10.1456474156918;
-    
     // check the values
     TEST_FLOATING_EQUALITY( normOne, controlNormOne, 1.0e-12 );
     TEST_FLOATING_EQUALITY( normInf, controlNormInf, 1.0e-12 );
@@ -85,7 +107,6 @@ TEUCHOS_UNIT_TEST( Ginla, KeoHashes )
     keoMatrix->Apply( e, Ke );
     double sum[1];
     e.Dot( Ke, sum );
-    double controlSum = -0.00844428504187249;
     TEST_FLOATING_EQUALITY( sum[0], controlSum, 1.0e-12 );
 
     // Sum over all the "real parts" of the matrix.
@@ -106,7 +127,6 @@ TEUCHOS_UNIT_TEST( Ginla, KeoHashes )
     Epetra_Vector t0( map );
     keoMatrix->Apply( s0, t0 );
     s0.Dot( t0, sum );
-    double controlSumReal = -0.0042221425209367988;
     TEST_FLOATING_EQUALITY( sum[0], controlSumReal, 1.0e-12 );
 
     // Sum over all the "imaginary parts" of the matrix.
@@ -123,9 +143,51 @@ TEUCHOS_UNIT_TEST( Ginla, KeoHashes )
     s1.Dot( t0, sum );
     // The matrix is Hermitian, so just test that the sum of
     // the imaginary parts is (close to) 0.
+    // Don't use TEST_FLOATING_EQUALITY as this one checks
+    // the *relative* error.
     TEST_COMPARE( fabs(sum[0]), <, 1.0e-12 );
 
     return;
+}
+// ===========================================================================
+TEUCHOS_UNIT_TEST( Ginla, KeoCubeSmallHashes )
+{
+    std::string inputFileNameBase = "cubesmall";
+
+    double mu = 1.0e-2;
+    double controlNormOne = 10.1456474156918;
+    double controlNormInf = 10.1456474156918;
+    double controlSum     = -0.00844428504187249;
+    double controlSumReal = -0.0042221425209367988;
+
+    testKeo( inputFileNameBase,
+             mu,
+             controlNormOne,
+             controlNormInf,
+             controlSum,
+             controlSumReal,
+             out,
+             success );
+}
+// ============================================================================
+TEUCHOS_UNIT_TEST( Ginla, KeoCubeLargeHashes )
+{
+    std::string inputFileNameBase = "cubelarge";
+
+    double mu = 1.0e-2;
+    double controlNormOne = 4.15276626198951;
+    double controlNormInf = 4.15276626198951;
+    double controlSum     = -0.834815525936051;
+    double controlSumReal = -0.417407762968018;
+
+    testKeo( inputFileNameBase,
+             mu,
+             controlNormOne,
+             controlNormInf,
+             controlSum,
+             controlSumReal,
+             out,
+             success );
 }
 // ============================================================================
 } // namespace
