d118715e8e8a340ffe91ff2c460dde2a47c3fd21
Nico Schlömer <nico.schloemer@gmail.com>
2012-04-04 13:19:36 +0200

    added configure script for trilinos on fuji

---
+++ sampleScripts/trilinos/do-configure.fuji.mpi.release.sh
@@ -0,0 +1,21 @@
+EXTRA_ARGS=$@
+TRILINOS_HOME=../../trilinos-10.10.0-Source
+
+cmake \
+  -D CMAKE_INSTALL_PREFIX:PATH=/opt/trilinos/10.10.0/openmpi/1.4.3/gcc/4.6.1/release \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D TPL_ENABLE_MPI:BOOL=ON \
+  -D Trilinos_ENABLE_TEUCHOS_TIME_MONITOR=ON \
+  -D Trilinos_ENABLE_NOX:BOOL=ON \
+      -D NOX_ENABLE_LOCA:BOOL=ON \
+  -D Trilinos_ENABLE_Piro:BOOL=ON \
+  -D Trilinos_ENABLE_ML:BOOL=ON \
+  -D Trilinos_ENABLE_Anasazi:BOOL=ON \
+  -D Trilinos_ENABLE_STK:BOOL=ON \
+  -D Trilinos_ENABLE_SEACASIoss:BOOL=ON \
+  -D TPL_ENABLE_Netcdf:BOOL=ON \
+      -D Netcdf_LIBRARY_DIRS:PATH=/opt/netcdf/4.1.2/lib/ \
+      -D TPL_Netcdf_INCLUDE_DIRS:PATH=/opt/netcdf/4.1.2/include \
+  -D DART_TESTING_TIMEOUT:STRING=600 \
+  $EXTRA_ARGS \
+  ${TRILINOS_HOME}
