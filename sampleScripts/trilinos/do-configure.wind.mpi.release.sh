991b684464245336619b9cf4d0bf3c19eceda5a9
Nico Schlömer <nico.schloemer@gmail.com>
2012-08-07 18:45:43 +0200

    update on configure scripts

--- sampleScripts/trilinos/do-configure.wind.mpi.release.sh
+++ sampleScripts/trilinos/do-configure.wind.mpi.release.sh
@@ -1,7 +1,3 @@
-EXTRA_ARGS=$@
-#TRILINOS_HOME=../../trilinos-10.10.2-Source
-TRILINOS_HOME=../../source
-
 cmake \
   -D CMAKE_INSTALL_PREFIX:PATH=/opt/trilinos/dev/master/openmpi/1.4.3/gcc/4.6.3/release \
   -D CMAKE_BUILD_TYPE=Release \
@@ -15,8 +11,6 @@ cmake \
   -D Trilinos_ENABLE_STK:BOOL=ON \
   -D Trilinos_ENABLE_SEACASIoss:BOOL=ON \
   -D TPL_ENABLE_Netcdf:BOOL=ON \
-      -D Netcdf_LIBRARY_DIRS:PATH=/opt/netcdf/4.1.2/lib/ \
-      -D TPL_Netcdf_INCLUDE_DIRS:PATH=/opt/netcdf/4.1.2/include \
-  -D DART_TESTING_TIMEOUT:STRING=600 \
-  $EXTRA_ARGS \
-  ${TRILINOS_HOME}
+      -D Netcdf_LIBRARY_DIRS:PATH=/opt/netcdf/4.2/lib/ \
+      -D TPL_Netcdf_INCLUDE_DIRS:PATH=/opt/netcdf/4.2/include \
+  ../../source
