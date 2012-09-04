86d032662a41a1c479218df475526e517964d383
Nico Schlömer <nico.schloemer@gmail.com>
2012-09-04 18:47:30 +0200

    more CMake fixes

--- sampleScripts/nosh/do-configure.fuji.mpi.release.sh
+++ sampleScripts/nosh/do-configure.fuji.mpi.release.sh
@@ -1,6 +1,5 @@
 #CMAKE_PREFIX_PATH=/opt/trilinos/dev/master/openmpi/1.4.3/gcc/4.7.1/release/:$CMAKE_PREFIX_PATH \
-#    -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/master/openmpi/1.4.3/gcc/4.7.1/release \
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/master/openmpi/1.4.3/gcc/4.7.1/release \
     ../../source/
-#LDFLAGS="-Wl,--as-needed" \
