fa62a1207a2fdfafde977c8fb6d33456ac126ded
Nico Schlömer <nico.schloemer@gmail.com>
2012-09-05 12:04:00 +0200

    move over some scripts to python-toolbox

--- tools/png2avi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# from http://www.mplayerhq.hu/DOCS/HTML/en/menc-feat-enc-images.html
-
-# trim the images
-mkdir test2
-mogrify -path ./test2 -format png -trim *.png
-
-mencoder mf://test2/*.png -mf w=800:h=600:fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi
