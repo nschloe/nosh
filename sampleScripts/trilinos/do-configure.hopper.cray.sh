194fdec565540f9182cdca604d8c29537f854ae6
Nico Schlömer <nico.schloemer@gmail.com>
2012-09-07 08:03:57 -0700

    updated hopper build scripts

--- sampleScripts/trilinos/do-configure.hopper.cray.sh
+++ sampleScripts/trilinos/do-configure.hopper.cray.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
+module load cmake/2.8.7
 module load boost
 module load binutils
-module load netcdf-hdf5parallel
+module load netcdf
 
-EXTRA_ARGS=$@
-TRILINOS_HOME=../../trilinos-10.10.2-Source
-
+#  -D Trilinos_SKIP_FORTRANCINTERFACE_VERIFY_TEST:BOOL=ON \
 cmake \
-  -D CMAKE_INSTALL_PREFIX:PATH="$SCRATCH/trilinos/10.10.2/cray/" \
-  -D Trilinos_VERBOSE_CONFIGURE:BOOL=OFF \
+  -D CMAKE_INSTALL_PREFIX:PATH="$SCRATCH/trilinos/dev/cray/" \
   -D CMAKE_BUILD_TYPE=Release \
+  -D Trilinos_ENABLE_DEVELOPMENT_MODE:BOOL=OFF \
   -D TPL_ENABLE_MPI:BOOL=ON \
       -D MPI_C_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/cc" \
       -D MPI_CXX_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/CC" \
       -D MPI_Fortran_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/ftn" \
   -D Trilinos_ENABLE_TEUCHOS_TIME_MONITOR:BOOL=ON \
-  -D Trilinos_SKIP_FORTRANCINTERFACE_VERIFY_TEST:BOOL=ON \
   -D TPL_ENABLE_Boost:BOOL=ON \
       -D Boost_LIBRARY_DIRS:PATH="$BOOST_DIR/libs" \
       -D Boost_INCLUDE_DIRS:PATH="$BOOST_DIR/include" \
   -D TPL_ENABLE_BLAS:BOOL=ON \
-      -D BLAS_LIBRARY_DIRS:PATH="$LIBSCI_BASE_DIR/cray73/mc12/lib" \
+      -D BLAS_LIBRARY_DIRS:PATH="$LIBSCI_BASE_DIR/cray/80/mc12/lib" \
       -D BLAS_LIBRARY_NAMES="sci_pgi" \
   -D TPL_ENABLE_LAPACK:BOOL=ON \
-      -D LAPACK_LIBRARY_DIRS:PATH="$LIBSCI_BASE_DIR/cray/73/mc12/lib" \
+      -D LAPACK_LIBRARY_DIRS:PATH="$LIBSCI_BASE_DIR/cray/80/mc12/lib" \
       -D LAPACK_LIBRARY_NAMES="sci_pgi" \
   -D Trilinos_ENABLE_NOX:BOOL=ON \
       -D NOX_ENABLE_LOCA:BOOL=ON \
@@ -35,8 +33,6 @@ cmake \
   -D Trilinos_ENABLE_Belos:BOOL=ON \
   -D Trilinos_ENABLE_SEACASIoss:BOOL=ON \
   -D TPL_ENABLE_Netcdf:BOOL=ON \
-      -D Netcdf_LIBRARY_DIRS:PATH="$CRAY_NETCDF_DIR/cray/73/lib" \
-      -D Netcdf_INCLUDE_DIRS:PATH="$CRAY_NETCDF_DIR/cray/73/include" \
-  -D DART_TESTING_TIMEOUT:STRING=600 \
-  $EXTRA_ARGS \
-  ${TRILINOS_HOME}
+      -D Netcdf_LIBRARY_DIRS:PATH="$CRAY_NETCDF_DIR/cray/80/lib" \
+      -D Netcdf_INCLUDE_DIRS:PATH="$CRAY_NETCDF_DIR/cray/80/include" \
+  ../../source/
--- sampleScripts/trilinos/do-configure.hopper.gnu.sh
+++ sampleScripts/trilinos/do-configure.hopper.gnu.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-#module swap PrgEnv-pgi PrgEnv-gnu
-#  -D BinUtils_LIBRARY_DIRS:PATH="$BINUTILS_DIR/lib" \
-
+module load cmake
 module load boost
 module load binutils
-module load netcdf-hdf5parallel
-
-EXTRA_ARGS=$@
-TRILINOS_HOME=../../source
+module load netcdf
 
 cmake \
   -D CMAKE_INSTALL_PREFIX:PATH="$SCRATCH/trilinos/dev/master/gnu/" \
   -D Trilinos_ENABLE_TEUCHOS_TIME_MONITOR:BOOL=ON \
+  -D Trilinos_ENABLE_DEVELOPMENT_MODE:BOOL=OFF \
   -D CMAKE_BUILD_TYPE=Release \
   -D TPL_ENABLE_MPI:BOOL=ON \
       -D MPI_C_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/cc" \
       -D MPI_CXX_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/CC" \
       -D MPI_Fortran_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/ftn" \
   -D TPL_ENABLE_BLAS:BOOL=ON \
-      -D BLAS_LIBRARY_DIRS:FILEPATH="$LIBSCI_BASE_DIR/gnu/46/mc12/lib" \
+      -D BLAS_LIBRARY_DIRS:FILEPATH="$LIBSCI_BASE_DIR/gnu/47/mc12/lib" \
       -D BLAS_LIBRARY_NAMES="sci_gnu" \
   -D TPL_ENABLE_LAPACK:BOOL=ON \
-      -D LAPACK_LIBRARY_DIRS:FILEPATH="$LIBSCI_BASE_DIR/gnu/46/mc12/lib" \
+      -D LAPACK_LIBRARY_DIRS:FILEPATH="$LIBSCI_BASE_DIR/gnu/47/mc12/lib" \
       -D LAPACK_LIBRARY_NAMES="sci_gnu" \
   -D TPL_ENABLE_Boost:BOOL=ON \
       -D Boost_LIBRARY_DIRS:PATH="$BOOST_DIR/libs" \
       -D Boost_INCLUDE_DIRS:PATH="$BOOST_DIR/include" \
   -D TPL_ENABLE_Netcdf:BOOL=ON \
-      -D Netcdf_LIBRARY_DIRS:FILEPATH="$CRAY_NETCDF_DIR/gnu/46/lib" \
-      -D Netcdf_INCLUDE_DIRS:FILEPATH="$CRAY_NETCDF_DIR/gnu/46/include" \
+      -D Netcdf_LIBRARY_DIRS:FILEPATH="$CRAY_NETCDF_DIR/gnu/47/lib" \
+      -D Netcdf_INCLUDE_DIRS:FILEPATH="$CRAY_NETCDF_DIR/gnu/47/include" \
   -D Trilinos_ENABLE_NOX:BOOL=ON \
       -D NOX_ENABLE_LOCA:BOOL=ON \
   -D Trilinos_ENABLE_Piro:BOOL=ON \
@@ -38,6 +34,4 @@ cmake \
   -D Trilinos_ENABLE_STK:BOOL=ON \
   -D Trilinos_ENABLE_Anasazi:BOOL=ON \
   -D Trilinos_ENABLE_SEACASIoss:BOOL=ON \
-  -D DART_TESTING_TIMEOUT:STRING=600 \
-  $EXTRA_ARGS \
-  ${TRILINOS_HOME}
+  ../../source
