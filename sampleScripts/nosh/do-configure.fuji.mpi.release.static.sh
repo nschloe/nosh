8a23e9d2a493a8621445043aa94dd402fb7a7f50
Nico Schlömer <nico.schloemer@gmail.com>
2012-09-21 17:00:13 +0200

    shared linking of TPLs even for static builds

--- sampleScripts/nosh/do-configure.fuji.mpi.release.static.sh
+++ sampleScripts/nosh/do-configure.fuji.mpi.release.static.sh
@@ -2,6 +2,7 @@ CMAKE_PREFIX_PATH=/opt/trilinos/dev/openmpi/1.4.3/gcc/4.7.1/release/static:$CMAK
 cmake \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_CXX_COMPILER:STRING=mpicxx \
+    -DCMAKE_Fortran_COMPILER:STRING=mpif90 \
     -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/master/openmpi/1.4.3/gcc/4.7.1/release/static \
     ../../source/
 #    -DBUILD_SHARED_LIBS:BOOL=TRUE \
--- sampleScripts/trilinos/do-configure.fuji.mpi.release.static.sh
+++ sampleScripts/trilinos/do-configure.fuji.mpi.release.static.sh
@@ -15,12 +15,18 @@
 # as well.
 # This of course is necessary only for statically linked libraries.
 
+# Okay, okay.
+# Ideally, we would like to link against the static version of the TPLs using
+#  -D TPL_FIND_SHARED_LIBS:BOOL=OFF.
+# However, this is just asking for trouble at this point (2012/09/15).
+# One of the problems is with Pthreads (Sandia bug https://software.sandia.gov/bugzilla/show_bug.cgi?id=5715),
+# another with netCDF's possible dependency on HDF5,
+#  -D Trilinos_EXTRA_LINK_FLAGS:STRING="-lhdf5_hl -lhdf5 -lz -lm -lcurl".
 cmake \
   -D CMAKE_INSTALL_PREFIX:PATH=/opt/trilinos/dev/openmpi/1.4.3/gcc/4.7.1/release/static \
   -D Trilinos_ENABLE_DEVELOPMENT_MODE:BOOL=OFF \
   -D CMAKE_BUILD_TYPE:STRING=Release \
   -D BUILD_SHARED_LIBS:BOOL=OFF \
-  -D TPL_FIND_SHARED_LIBS:BOOL=OFF \
   -D TPL_ENABLE_MPI:BOOL=ON \
   -D Trilinos_ENABLE_TEUCHOS_TIME_MONITOR:BOOL=ON \
   -D Trilinos_ENABLE_NOX:BOOL=ON \
@@ -34,7 +40,6 @@ cmake \
   -D Trilinos_ENABLE_SEACASNemslice:BOOL=ON \
   -D Trilinos_ENABLE_SEACASNemspread:BOOL=ON \
   -D TPL_ENABLE_Netcdf:BOOL=ON \
-  -D Trilinos_EXTRA_LINK_FLAGS:STRING="-lhdf5_hl -lhdf5 -lz -lm -lcurl" \
   ../../source
 
 #  -D Trilinos_INSTALL_INCLUDE_DIR:PATH=include/trilinos \
