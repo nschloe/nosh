05cafbf8afc84962215af87db2cd473aae1ef099
Nico Schlömer <nico.schloemer@gmail.com>
2012-10-24 15:35:40 +0200

    remove unneeded ${Trilinos_TPL_LIBRARIES}

--- sampleScripts/nosh/do-configure.fuji.mpi.debug.shared.sh
+++ sampleScripts/nosh/do-configure.fuji.mpi.debug.shared.sh
@@ -1,8 +1,9 @@
-CMAKE_PREFIX_PATH=/opt/trilinos/dev/openmpi/1.4.5/gcc/4.7.2/debug/shared:$CMAKE_PREFIX_PATH \
+#CMAKE_PREFIX_PATH=/opt/trilinos/dev/openmpi/1.4.5/gcc/4.7.2/debug/shared:$CMAKE_PREFIX_PATH \
 cmake \
     -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/openmpi/1.4.5/gcc/4.7.2/debug/shared/ \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_CXX_COMPILER:STRING=mpic++ \
     -DCMAKE_Fortran_COMPILER:STRING=mpif90 \
     -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DCMAKE_SHARED_LINKER_FLAGS:STRING="-Wl,--no-undefined" \
     ../../source/
