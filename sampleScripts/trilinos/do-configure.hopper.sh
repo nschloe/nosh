520e19ea2b78efea7363ebf14717c25351d4728b
Nico Schlömer <nico.schloemer@gmail.com>
2013-03-29 10:59:33 -0700

    unify hopper build scripts

--- sampleScripts/trilinos/do-configure.hopper.sh
+++ sampleScripts/trilinos/do-configure.hopper.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-# Make sure the correct compiler is currently loaded.
-if [ "$CRAY_PRGENVCRAY" != "loaded" ]; then
-echo "Incorrect compiler. Abort."
-exit 1
+# Check which compiler is loadad.
+if [ "$CRAY_PRGENVPGI" == "loaded" ]; then
+  echo "Using PGI compilers."
+  NAME="pgi"
+  SCILIB_NAME="sci_pgi"
+elif [ "$CRAY_PRGENVCRAY" == "loaded" ]; then
+  echo "Using Cray compilers."
+  NAME="cray"
+  SCILIB_NAME="sci_cray"
+elif [ "$CRAY_PRGENVGNU" == "loaded" ]; then
+  echo "Using GNU compilers."
+  NAME="gnu"
+  SCILIB_NAME="sci_gnu"
+elif [ "$CRAY_PRGENVINTEL" == "loaded" ]; then
+  echo "Using Intel compilers."
+  NAME="intel"
+  SCILIB_NAME="sci_intel"
+elif [ "$CRAY_PRGENVPATHSCALE" == "loaded" ]; then
+  echo "Using PathScale compilers."
+  NAME="pathscale"
+  SCILIB_NAME="sci_pathscale"
+else
+  echo "Unknown compiler suite selected. Abort."
+  exit 1
 fi
 
-module load cmake/2.8.9
+# Load libraries needed for building.
+module load cmake
 module load boost/1.50
 module load binutils
-module load netcdf
+
+# Serial netCDF is indeed sufficient if
+#  -D SEACASExodus_ENABLE_MPI:BOOL=OFF
+# is set.
+echo ${NETCDF_DIR?Error Need netCDF loaded (module load netcdf).}
 
 # Use TPL_FIND_SHARED_LIBS:BOOL=OFF to force Trilinos to find
 # the static versions of the TPLs instead of their shared
-# counterparts
+# counterparts.
 
 cmake \
-  -D CMAKE_INSTALL_PREFIX:PATH="$SCRATCH/trilinos/dev/cray/" \
-  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_INSTALL_PREFIX:PATH="$SCRATCH/trilinos/dev/${NAME}/" \
+  -D CMAKE_BUILD_TYPE:STRING=Release \
   -D Trilinos_ENABLE_DEVELOPMENT_MODE:BOOL=OFF \
   -D BUILD_SHARED_LIBS:BOOL=OFF \
   -D TPL_FIND_SHARED_LIBS:BOOL=OFF \
@@ -25,17 +50,17 @@ cmake \
       -D MPI_C_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/cc" \
       -D MPI_CXX_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/CC" \
       -D MPI_Fortran_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/ftn" \
-  -D Trilinos_SKIP_FORTRANCINTERFACE_VERIFY_TEST:BOOL=ON \
+  -D Trilinos_SKIP_FORTRANCINTERFACE_VERIFY_TEST:BOOL=OFF \
   -D Trilinos_ENABLE_TEUCHOS_TIME_MONITOR:BOOL=ON \
   -D TPL_ENABLE_Boost:BOOL=ON \
       -D Boost_LIBRARY_DIRS:PATH="$BOOST_DIR/libs" \
       -D Boost_INCLUDE_DIRS:PATH="$BOOST_DIR/include" \
   -D TPL_ENABLE_BLAS:BOOL=ON \
       -D BLAS_LIBRARY_DIRS:PATH="$CRAY_LIBSCI_PREFIX_DIR/lib" \
-      -D BLAS_LIBRARY_NAMES="sci_cray" \
+      -D BLAS_LIBRARY_NAMES:STRING="${SCILIB_NAME}" \
   -D TPL_ENABLE_LAPACK:BOOL=ON \
       -D LAPACK_LIBRARY_DIRS:PATH="$CRAY_LIBSCI_PREFIX_DIR/lib" \
-      -D LAPACK_LIBRARY_NAMES="sci_cray" \
+      -D LAPACK_LIBRARY_NAMES:STRING="${SCILIB_NAME}" \
   -D Trilinos_ENABLE_NOX:BOOL=ON \
       -D NOX_ENABLE_LOCA:BOOL=ON \
   -D Trilinos_ENABLE_Piro:BOOL=ON \
@@ -44,8 +69,8 @@ cmake \
   -D Trilinos_ENABLE_STK:BOOL=ON \
   -D Trilinos_ENABLE_Belos:BOOL=ON \
   -D Trilinos_ENABLE_SEACASIoss:BOOL=ON \
+  -D SEACASExodus_ENABLE_MPI:BOOL=OFF \
   -D TPL_ENABLE_Netcdf:BOOL=ON \
       -D Netcdf_INCLUDE_DIRS:PATH="$NETCDF_DIR/include" \
       -D Netcdf_LIBRARY_DIRS:PATH="$NETCDF_DIR/lib" \
-      -D Netcdf_LIBRARY_NAMES="netcdf" \
   ../../source/
