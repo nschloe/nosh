d4fb3a069a741b07fbe8a49a1cfd4c9dde506d7f
Nico Schlömer <nico.schloemer@gmail.com>
2013-06-24 12:03:40 -0700

    improve build script for hopper

--- sampleScripts/trilinos/do-configure.hopper.sh
+++ sampleScripts/trilinos/do-configure.hopper.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
 
+# Make sure CMake is loaded.
+command -v cmake >/dev/null 2>&1 || { echo "cmake not found. Try 'module load cmake'." >&2; exit 1; }
+
+# Serial netCDF is indeed sufficient if
+#  -D SEACASExodus_ENABLE_MPI:BOOL=OFF
+# is set.
+# echo ${NETCDF_DIR?Error: Need netCDF loaded (module load netcdf).}
+echo ${CRAY_NETCDF_HDF5PARALLEL_VERSION?Error: Need parallel netCDF loaded (module load netcdf-hdf5parallel).}
+
+echo ${BOOST_DIR?Error: Need Boost loaded (module load boost/1.50).}
+
 # Check which compiler is loadad.
 if [ "$CRAY_PRGENVPGI" == "loaded" ]; then
   echo "Using PGI compilers."
   NAME="pgi"
   SCILIB_NAME="sci_pgi"
-  SKIP_CF="ON"
+  BINUTILS="OFF"
+  SKIP_C_FORTRAN="ON"
 elif [ "$CRAY_PRGENVCRAY" == "loaded" ]; then
   echo "Using Cray compilers."
   NAME="cray"
   SCILIB_NAME="sci_cray"
-  SKIP_CF="OFF"
+  BINUTILS="OFF"
+  SKIP_C_FORTRAN="OFF"
 elif [ "$CRAY_PRGENVGNU" == "loaded" ]; then
   echo "Using GNU compilers."
+  echo ${BINUTILS_DIR?Error: Need Binutils loaded (module load binutils).}
   NAME="gnu"
   SCILIB_NAME="sci_gnu"
-  SKIP_CF="OFF"
+  BINUTILS="ON"
+  SKIP_C_FORTRAN="OFF"
 elif [ "$CRAY_PRGENVINTEL" == "loaded" ]; then
   echo "Using Intel compilers."
   NAME="intel"
   SCILIB_NAME="sci_intel"
-  SKIP_CF="OFF"
+  BINUTILS="OFF"
+  SKIP_C_FORTRAN="OFF"
 elif [ "$CRAY_PRGENVPATHSCALE" == "loaded" ]; then
   echo "Using PathScale compilers."
   NAME="pathscale"
   SCILIB_NAME="sci_pathscale"
-  SKIP_CF="OFF"
+  BINUTILS="OFF"
+  SKIP_C_FORTRAN="OFF"
 else
   echo "Unknown compiler suite selected. Abort."
   exit 1
@@ -34,21 +51,23 @@ fi
 # Give the user some time to ctrl-c out. :)
 sleep 5
 
-# Load libraries needed for building.
-module load boost/1.50
-module load binutils
 
-# Make sure CMake is loaded.
-command -v cmake >/dev/null 2>&1 || { echo "cmake not found. Try 'module load cmake'." >&2; exit 1; }
+# Use
+# TPL_FIND_SHARED_LIBS:BOOL=OFF,
+# Trilinos_LINK_SEARCH_START_STATIC:BOOL=ON
+# to force Trilinos to find the static versions of the TPLs
+# instead of their shared counterparts.
 
-# Serial netCDF is indeed sufficient if
-#  -D SEACASExodus_ENABLE_MPI:BOOL=OFF
-# is set.
-echo ${NETCDF_DIR?Error Need netCDF loaded (module load netcdf).}
-
-# Use TPL_FIND_SHARED_LIBS:BOOL=OFF to force Trilinos to find
-# the static versions of the TPLs instead of their shared
-# counterparts.
+# On hopper, BLAS and LAPACK are implicitly provided by the compiler iff
+# the cray-libsci module is loaded.
+# We could set  TPL_{BLAS,LAPACK}_LIBRARIES to " " in this case, and
+# ignore the compiler warnings
+#
+#   ignoring option '-l'; argument required.
+#
+# We can also just explicitly link the libraries again.
+#
+echo ${CRAY_LIBSCI_PREFIX_DIR?Error: Need libsci loaded (module load cray-libsci).}
 
 cmake \
   -D CMAKE_INSTALL_PREFIX:PATH="$SCRATCH/trilinos/dev/${NAME}/" \
@@ -56,11 +75,15 @@ cmake \
   -D Trilinos_ENABLE_DEVELOPMENT_MODE:BOOL=OFF \
   -D BUILD_SHARED_LIBS:BOOL=OFF \
   -D TPL_FIND_SHARED_LIBS:BOOL=OFF \
+  -D Trilinos_LINK_SEARCH_START_STATIC:BOOL=ON \
+  -D Trilinos_ENABLE_TESTS:BOOL=ON \
+  -D TPL_ENABLE_BinUtils:BOOL=${BINUTILS} \
+  -D Trilinos_ENABLE_OpenMP:BOOL=ON \
   -D TPL_ENABLE_MPI:BOOL=ON \
       -D MPI_C_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/cc" \
       -D MPI_CXX_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/CC" \
       -D MPI_Fortran_COMPILER:FILEPATH="$ASYNCPE_DIR/bin/ftn" \
-  -D Trilinos_SKIP_FORTRANCINTERFACE_VERIFY_TEST:BOOL=${SKIP_CF} \
+  -D Trilinos_SKIP_FORTRANCINTERFACE_VERIFY_TEST:BOOL=${SKIP_C_FORTRAN} \
   -D Trilinos_ENABLE_TEUCHOS_TIME_MONITOR:BOOL=ON \
   -D TPL_ENABLE_Boost:BOOL=ON \
       -D Boost_LIBRARY_DIRS:PATH="$BOOST_DIR/libs" \
@@ -74,13 +97,12 @@ cmake \
   -D Trilinos_ENABLE_NOX:BOOL=ON \
       -D NOX_ENABLE_LOCA:BOOL=ON \
   -D Trilinos_ENABLE_Piro:BOOL=ON \
+  -D Trilinos_ENABLE_Belos:BOOL=ON \
   -D Trilinos_ENABLE_ML:BOOL=ON \
   -D Trilinos_ENABLE_Anasazi:BOOL=ON \
   -D Trilinos_ENABLE_STK:BOOL=ON \
-  -D Trilinos_ENABLE_Belos:BOOL=ON \
   -D Trilinos_ENABLE_SEACASIoss:BOOL=ON \
-  -D SEACASExodus_ENABLE_MPI:BOOL=OFF \
   -D TPL_ENABLE_Netcdf:BOOL=ON \
       -D Netcdf_INCLUDE_DIRS:PATH="$NETCDF_DIR/include" \
       -D Netcdf_LIBRARY_DIRS:PATH="$NETCDF_DIR/lib" \
-  ../../source/
+  ${HOME}/software/trilinos/dev/source/
