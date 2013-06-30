37d18b8cbfb983038fc494c53d250c2380a8c737
Nico Schlömer <nico.schloemer@gmail.com>
2013-06-30 16:07:03 +0200

    small cleanups for build system

--- sampleScripts/nosh/do-configure.fuji.mpi.debug.shared.sh
+++ sampleScripts/nosh/do-configure.fuji.mpi.debug.shared.sh
@@ -1,6 +1,6 @@
 #CMAKE_PREFIX_PATH=/opt/trilinos/dev/openmpi/1.4.5/gcc/4.7.2/debug/shared:$CMAKE_PREFIX_PATH \
 cmake \
-    -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/openmpi/1.4.5/gcc/4.7.2/debug/shared/ \
+    -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/openmpi/1.4.5/gcc/4.7.3/debug/shared/ \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_CXX_COMPILER:STRING=mpicxx \
     -DCMAKE_Fortran_COMPILER:STRING=mpif90 \
--- sampleScripts/trilinos/do-configure.fuji.mpi.release.shared.sh
+++ sampleScripts/trilinos/do-configure.fuji.mpi.release.shared.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+#  -D Trilinos_VERBOSE_CONFIGURE:BOOL=ON \
 cmake \
-  -D CMAKE_INSTALL_PREFIX:PATH=/opt/trilinos/dev/openmpi/1.4.5/gcc/4.7.2/release/shared \
+  -D CMAKE_INSTALL_PREFIX:PATH=/opt/trilinos/dev/openmpi/1.4.5/gcc/4.7.3/release/shared \
   -D CMAKE_SHARED_LINKER_FLAGS:STRING="-Wl,--no-undefined" \
   -D Trilinos_ENABLE_DEVELOPMENT_MODE:BOOL=OFF \
   -D CMAKE_BUILD_TYPE:STRING=Release \
@@ -17,11 +18,9 @@ cmake \
   -D Trilinos_ENABLE_Stokhos:BOOL=ON \
   -D Trilinos_ENABLE_STK:BOOL=ON \
   -D Trilinos_ENABLE_SEACASIoss:BOOL=ON \
-  -D Trilinos_ENABLE_SEACASNemslice:BOOL=ON \
-  -D Trilinos_ENABLE_SEACASNemspread:BOOL=ON \
+  -D SEACASExodus_ENABLE_MPI:BOOL=OFF \
   -D TPL_ENABLE_Netcdf:BOOL=ON \
   ../../source
 
-  
 #  -D Trilinos_INSTALL_INCLUDE_DIR:PATH=include/trilinos \
 #  -D CMAKE_BUILD_TYPE=None \
