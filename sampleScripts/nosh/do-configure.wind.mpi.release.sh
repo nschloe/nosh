856062ba954a5279a7ad1d64c3e4b867f268a077
Nico Schlömer <nico.schloemer@gmail.com>
2014-08-28 23:45:20 +0200

    added more files git .gitignore

--- sampleScripts/nosh/do-configure.wind.mpi.release.sh
+++ sampleScripts/nosh/do-configure.wind.mpi.release.sh
@@ -4,6 +4,7 @@ CMAKE_PREFIX_PATH=/opt/trilinos/coll/:$CMAKE_PREFIX_PATH \
 CXX=mpicxx \
 FC=mpif90 \
 cmake \
+    -DCMAKE_SHARED_LINKER_FLAGS:STRING="-Wl,--no-undefined" \
     -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/ \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DBUILD_SHARED_LIBS:BOOL=TRUE \
