e2bca0b574beeda94749496b5a63933777157ff1
Nico Schlömer <nico.schloemer@gmail.com>
2015-06-22 14:54:19 +0200

    use system trilinos

--- sampleScripts/nosh/do-configure.fuji.mpi.debug.shared.sh
+++ sampleScripts/nosh/do-configure.fuji.mpi.debug.shared.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CMAKE_PREFIX_PATH=/opt/trilinos/public/:$CMAKE_PREFIX_PATH \
+#CMAKE_PREFIX_PATH=/opt/trilinos/public/:$CMAKE_PREFIX_PATH \
 CMAKE_PREFIX_PATH=/opt/vtk/upstream/:$CMAKE_PREFIX_PATH \
 cmake \
     -DCMAKE_INSTALL_PREFIX:PATH=/opt/nosh/dev/openmpi/1.4.5/gcc/4.7.3/debug/shared/ \
