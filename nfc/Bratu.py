3862de53a725ae3e583bef5ee6c33940a906a460
Nico Schlömer <nico.schloemer@gmail.com>
2015-06-24 22:11:30 +0200

    don't hardcode 'Poisson' in nfc

---
+++ nfc/Bratu.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @HEADER
+#
+#    NFL file for the Bratu problem.
+#    Copyright (C) 2015  Nico Schlömer
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @HEADER
+#
+from nfl import *
+
+bc0 = DirichletBC(
+    lambda x: x[0] > -10000,
+    lambda x: 0.0
+    )
+
+A = EdgeOperator(
+    lambda alpha, c0, c1: [[alpha, -alpha],  [-alpha, alpha]]
+    )
+
+#F = NonlinearOperator(
+#    eval=(lambda u: A*u - lmbd * exp(u)),
+#    Jac=(lambda u0: A - lmbd * exp(u0))
+#    )
