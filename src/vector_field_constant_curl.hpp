14ac66e014f424a1b0e2fcfd6e97c1725d96c10f
Nico Schlömer <nico.schloemer@gmail.com>
2016-03-19 19:01:18 +0100

    remove license headers from files

--- src/vector_field_constant_curl.hpp
+++ src/vector_field_constant_curl.hpp
@@ -1,22 +1,3 @@
-// @HEADER
-//
-//    Query routines for the vector potential associated with a constant curl field.
-//    Copyright (C) 2011, 2012  Nico Schlömer
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// @HEADER
 #ifndef NOSH_VECTORFIELD_CONSTANTCURL_H_
 #define NOSH_VECTORFIELD_CONSTANTCURL_H_
 
