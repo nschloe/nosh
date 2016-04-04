23e5f8ca806b7ec63c20080528d4532c03ccfaba
Nico Schlömer <nico.schloemer@gmail.com>
2016-04-04 23:53:51 +0200

    first steps towards code generation

--- examples/singular-perturbation/singular.py
+++ examples/singular-perturbation/singular.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
-from nfl import *
 from sympy import *
+from nfl import *
 
 
 class Bc1(DirichletBC):
     def eval(self, x): return 0.0
 
 
-# class Singular(Operator):
-#     def eval(u):
-#         return - 2.0e-1 * dot(n, grad(u)) * domega + u * dV
-#
-#     boundary_conditions = [Bc1()]
+class Alpha(Expression):
+    @classmethod
+    def eval(cls, x): return sin(x[1])
+    degree = 0
 
 
-class Core0(MatrixCore):
-    def edge_contrib(self, x0, x1, edge_length, edge_covolume):
-        eps = 2.0e-1
-        alpha = edge_covolume / edge_length
-        return [[eps * alpha, -eps * alpha],
-                [-eps * alpha, eps * alpha]
-                ]
-
-    def vertex_contrib(self, x, control_volume):
-        return control_volume
-
-
-class Singular(FvmMatrix):
-    matrix_cores = [Core0()]
+class Singular(FvmMatrix2):
+    def eval(u):
+        return \
+              integrate(lambda x: dot(n, grad(u)), dS()) #+ \
+              # integrate(lambda x: u(x), dV())
+              # integrate(lambda x: 2 * sin(x[0]) * u(x), dV())
     boundary_conditions = [Bc1()]
+
+
+# class Core0(MatrixCore):
+#     def edge_contrib(self, x0, x1, edge_length, edge_covolume):
+#         eps = 2.0e-1
+#         alpha = edge_covolume / edge_length
+#         return [[eps * alpha, -eps * alpha],
+#                 [-eps * alpha, eps * alpha]
+#                 ]
+#
+#     def vertex_contrib(self, x, control_volume):
+#         return control_volume
+#
+# class Singular(FvmMatrix):
+#     matrix_cores = [Core0()]
+#     boundary_conditions = [Bc1()]
--- nfc/nfl.py
+++ nfc/nfl.py
@@ -79,10 +79,54 @@ class FvmMatrix(sympy.Function):
     #    self.edge_function = edge_function
     #    return
 
+class FvmMatrix2(sympy.Function):
+    pass
+
 
 class MatrixFactory(object):
     def __init__(self):
         return
+
+
+class Measure(object):
+    pass
+
+
+class dV(Measure):
+    pass
+
+
+class dS(Measure):
+    pass
+
+
+def integrate(integrand, measure):
+    assert(isinstance(measure, Measure))
+
+    if isinstance(measure, dV):
+        return Contributions(integrand, 0)
+    elif isinstance(measure, dS):
+        return Contributions(0, integrand)
+    else:
+        raise RuntimeError('Illegal measure')
+
+
+class Contributions(object):
+    def __init__(self, vertex, edge):
+        self.vertex = vertex
+        self.edge = edge
+
+    def __add__(self, other):
+        return Contributions(
+                lambda x: self.vertex(x) + other.vertex(x),
+                lambda x: self.edge(x) + other.edge(x)
+                )
+
+    def __sub__(self, other):
+        return Contributions(
+                lambda x: self.vertex(x) - other.vertex(x),
+                lambda x: self.edge(x) - other.edge(x)
+                )
 
 
 class NonlinearProblem(object):
