4a0cf7554b0891c967f3b683a845572f3ef601d5
Nico Schlömer <nico.schloemer@gmail.com>
2016-04-15 16:09:18 +0200

    proper status tests for nonlinear solver

--- examples/bratu/bratu.cpp
+++ examples/bratu/bratu.cpp
@@ -31,15 +31,32 @@ int main(int argc, char *argv[]) {
       model,
       {
         {"method", "Newton"},
-        {"NOX Status Test", list{
-          {"Test 1", list {
-            {"Test Type", "MaxIters"},
-            {"Maximum Iterations", 1}
-          }}
-        }},
         {"NOX", list{
-          {"Tolerance", 1.0e2},
-          {"Max steps", 1},
+          {"Status Tests", list{
+            {"Test Type", "Combo"},
+            {"Combo Type", "OR"},
+            {"Number of Tests", 2},
+            {"Test 0", list{
+              {"Test Type", "Combo"},
+              {"Combo Type", "AND"},
+              {"Number of Tests", 2},
+              {"Test 0", list{
+                {"Test Type", "NormF"},
+                {"Norm Type", "Two Norm"},
+                {"Scale Type", "Scaled"},
+                {"Tolerance", 1.0e-8}
+              }},
+              {"Test 1", list{
+                {"Test Type", "NormWRMS"},
+                {"Absolute Tolerance", 1.0e-6},
+                {"Relative Tolerance", 1.0e-6}
+              }},
+            }},
+            {"Test 1", list {
+              {"Test Type", "MaxIters"},
+              {"Maximum Iterations", 5}
+            }}
+          }},
           {"Printing", list{
            {"Output Information", list{
              {"Details", true},
@@ -60,50 +77,6 @@ int main(int argc, char *argv[]) {
         }},
       }
       );
-    // <ParameterList name="NOX Status Test" >
-    //     <Parameter name="Test Type" type="string" value="Combo"/>
-    //     <Parameter name="Number of Tests" type="int" value="4"/>
-    //     <Parameter name="Combo Type" type="string" value="OR"/>
-    //     <ParameterList name="Test 0">
-    //         <Parameter name="Test Type" type="string" value="Combo"/>
-    //         <Parameter name="Number of Tests" type="int" value="3"/>
-    //         <Parameter name="Combo Type" type="string" value="AND"/>
-    //         <ParameterList name="Test 0">
-    //             <Parameter name="Test Type" type="string" value="NormF"/>
-    //             <Parameter name="Norm Type" type="string" value="Two Norm"/>
-    //             <Parameter name="Tolerance" type="double" value="1.0e-8"/>
-    //             <Parameter name="Scale Type" type="string" value="Unscaled"/>
-    //         </ParameterList>
-    //         <ParameterList name="Test 1">
-    //             <Parameter name="Test Type" type="string" value="NormUpdate"/>
-    //             <Parameter name="Norm Type" type="string" value="Two Norm"/>
-    //             <Parameter name="Tolerance" type="double" value="1.0e-8"/>
-    //             <Parameter name="Scale Type" type="string" value="Unscaled"/>
-    //         </ParameterList>
-    //         <ParameterList name="Test 2">
-    //             <Parameter name="Test Type" type="string" value="NormWRMS"/>
-    //             <Parameter name="BDF Multiplier" type="double" value="1.0"/>
-    //             <Parameter name="Tolerance" type="double" value="1.0"/>
-    //             <Parameter name="Alpha" type="double" value="1.0"/>
-    //             <Parameter name="Beta" type="double" value="0.5"/>
-    //             <Parameter name="Relative Tolerance" type="double" value="1.0e-5"/>
-    //             <Parameter name="Absolute Tolerance" type="double" value="1.0e-8"/>
-    //         </ParameterList>
-    //     </ParameterList>
-    //     <ParameterList name="Test 1">
-    //         <Parameter name="Test Type" type="string" value="MaxIters"/>
-    //         <Parameter name="Maximum Iterations" type="int" value="25"/>
-    //     </ParameterList>
-    //     <ParameterList name="Test 2">
-    //         <Parameter name="Test Type" type="string" value="NormF"/>
-    //         <Parameter name="Norm Type" type="string" value="Two Norm"/>
-    //         <Parameter name="Tolerance" type="double" value="1.0e-13"/>
-    //         <Parameter name="Scale Type" type="string" value="Unscaled"/>
-    //     </ParameterList>
-    //     <ParameterList name="Test 3">
-    //         <Parameter name="Test Type" type="string" value="FiniteValue"/>
-    //     </ParameterList>
-    // </ParameterList>
 
 #if 0
   // nosh::nonlinear_solve(
